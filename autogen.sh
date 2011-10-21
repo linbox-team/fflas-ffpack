925728285f2a6b7c4e95780f954b87cce291151a
bboyer <bboyer@ba0639da-81a4-4d54-96fc-8253e07fa939>
2011-10-21 12:54:55 +0000

    sync autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # This file is part of FFLAS-FFPACK.
 # See COPYING for details about the licence.
+
 # Run this to generate all the initial makefiles, etc.
 
 
@@ -19,7 +20,6 @@ done
 
 echo  "$0 $CMDLINE" > autogen.status
 chmod +x autogen.status
-
 
 # Starts configuring
 srcdir=`dirname $0`
@@ -53,28 +53,21 @@ LIBTOOL=glibtool
 LIBTOOLIZE=glibtoolize
 }
 
+
 (autoconf --version) < /dev/null > /dev/null 2>&1 || {
-echo
-echo "You must have autoconf installed to compile $PROJECT."
-echo "Download the appropriate package for your distribution,"
-echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
-DIE=1
+	echo
+	echo "You must have autoconf installed to compile $PROJECT."
+	echo "Download the appropriate package for your distribution,"
+	echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
+	DIE=1
 }
 
 (automake --version) < /dev/null > /dev/null 2>&1 || {
-echo
-echo "You must have automake installed to compile $PROJECT."
-echo "Download the appropriate package for your distribution,"
-echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
-DIE=1
- }
-
-($LIBTOOL --version) < /dev/null > /dev/null 2>&1 || {
- echo
- echo "You must have libtool installed to compile $PROJECT."
- echo "Download the appropriate package for your distribution,"
- echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
- DIE=1
+	echo
+	echo "You must have automake installed to compile $PROJECT."
+	echo "Download the appropriate package for your distribution,"
+	echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
+	DIE=1
 }
 
 (automake --version) < /dev/null > /dev/null 2>&1 || {
@@ -88,10 +81,10 @@ DIE=1
 (grep "^AC_PROG_LIBTOOL" configure.ac >/dev/null) && {
   ($LIBTOOL --version) < /dev/null > /dev/null 2>&1 || {
      echo
-	 echo "You must have libtool installed to compile $PROJECT."
-	 echo "Download the appropriate package for your distribution,"
-	 echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
-	 DIE=1
+     echo "**Error**: You must have \`libtool' installed to compile $PROJECT."
+     echo "Download the appropriate package for your distribution,"
+     echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
+     DIE=1
   }
 }
 
@@ -99,7 +92,7 @@ grep "^AM_GNU_GETTEXT" configure.ac >/dev/null && {
   grep "sed.*POTFILES" $srcdir/configure.ac >/dev/null || \
 	(gettext --version) < /dev/null > /dev/null 2>&1 || {
        echo
-	   echo "You must have gettext installed to compile $PROJECT."
+	   echo "**Error**: You must have \`gettext' installed to compile $PROJECT."
 	   echo "Download the appropriate package for your distribution,"
 	   echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
 	   DIE=1
@@ -176,7 +169,6 @@ do
 done
 
 conf_flags="--enable-maintainer-mode" 
-#--enable-compile-warnings
 #--enable-iso-c
 
 cd "$ORIGDIR"
