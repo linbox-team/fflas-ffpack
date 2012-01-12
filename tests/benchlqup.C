1485f03d6cca533e1950596a901756a6a8c601a7
bboyer <bboyer@ba0639da-81a4-4d54-96fc-8253e07fa939>
2012-01-12 12:33:18 +0000

    update licence to LGPL 2.1+ (2/n)

--- tests/benchlqup.C
+++ tests/benchlqup.C
@@ -1,5 +1,28 @@
 /* -*- mode: C++; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
 // vim:sts=8:sw=8:ts=8:noet:sr:cino=>s,f0,{0,g0,(0,\:0,t0,+0,=s
+//
+/*
+ * Coypright (c) FFLAS-FFPACK
+ * Written by Clement Pernet <clement.pernet@imag.fr>
+ *
+ * ========LICENCE========
+ * This file is part of the library FFLAS-FFPACK.
+ *
+ * FFLAS-FFPACK is free software: you can redistribute it and/or modify
+ * it under the terms of the  GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * ========LICENCE========
+ */
 #include <iostream>
 
 #include "fflas-ffpack/ffpack/ffpack.h"
--- tests/test-ftrtri.C
+++ tests/test-ftrtri.C
@@ -3,29 +3,28 @@
 
 /*
  * Copyright (C) FFLAS-FFPACK
- * Written by Clément Pernet
+ * Written by Clément Pernet <clement.pernet@imag.fr>
  * This file is Free Software and part of FFLAS-FFPACK.
- * 
+ *
  * ========LICENCE========
  * This file is part of the library FFLAS-FFPACK.
- * 
+ *
  * FFLAS-FFPACK is free software: you can redistribute it and/or modify
  * it under the terms of the  GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  * ========LICENCE========
  *.
  */
-
 
 //--------------------------------------------------------------------------
 //                        Test for ftrtri : 1 computation
