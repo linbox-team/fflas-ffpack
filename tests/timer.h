3a8a8f2a58e219e52eab5a2c3de93ed25478bb1a
bboyer <bboyer@ba0639da-81a4-4d54-96fc-8253e07fa939>
2012-03-02 10:25:11 +0000

    put OMP timer somewhere

--- tests/timer.h
+++ tests/timer.h
@@ -17,27 +17,27 @@
  * integrated into FFLAS_FFPACK
  *
  * ------------------------------------
- * 
+ *
  * ========LICENCE========
  * This file is part of the library FFLAS-FFPACK.
- * 
+ *
  * FFLAS-FFPACK is free software: you can redistribute it and/or modify
  * it under the terms of the  GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  * ========LICENCE========
  *.
  *
- * This file implements the C++ interface to commentators (for 
+ * This file implements the C++ interface to commentators (for
  * providing runtime commentary to the user)
  */
 
@@ -46,16 +46,16 @@
 
 #include <iostream>
 
-class BaseTimer { 
+class BaseTimer {
     public:
-	enum { 
+	enum {
 		MSPSEC = 1000000  // microsecond per second
 	};
 
 	// -- Clear timer :
 	inline void clear() { _t = 0; }
 
-	// -- total amount of second spent 
+	// -- total amount of second spent
 	inline double time() const { return _t; }
 
 	// -- Return a value to initialize random generator
@@ -63,7 +63,7 @@ class BaseTimer {
 
 	// -- basic methods:
 	std::ostream &print (std::ostream &) const;
-       
+
 	// -- Some arithmetic operators to compute cumulative time :
 	BaseTimer& operator = (const BaseTimer & T) ;
 	const BaseTimer operator - (const BaseTimer & T)  const;
@@ -74,7 +74,7 @@ class BaseTimer {
 
     public:
 	double _start_t;  // time as of start ()
-	double _t;        // time  
+	double _t;        // time
 };
 
 inline std::ostream &operator << (std::ostream &o, const BaseTimer &BT)
@@ -111,12 +111,12 @@ class Timer {
     public :
 
 	// Clear timer :
-	void clear(); 
+	void clear();
 
 	// Start timer
 	void start ();
 
-	// Stop timer 
+	// Stop timer
 	void stop ();
 
 	// total amount of second spent in user mode
@@ -125,7 +125,7 @@ class Timer {
 	// total amount of second spent in system mode
 	double systime () const { return st.time(); }
 
-	// real total amount of second spent.  
+	// real total amount of second spent.
 	double realtime () const { return rt.time(); }
 
 	// retourne une petite graine
@@ -145,7 +145,7 @@ class Timer {
 	size_t count() const {return _count;}
 
     private:
-	size_t _count; // how many 
+	size_t _count; // how many
 
 	RealTimer rt;
 	UserTimer ut;
@@ -156,13 +156,43 @@ class Timer {
 // 	{ return T.print (o); }
 
 inline std::ostream &operator << (std::ostream &o, const Timer &T)
-{ 
+{
 	double ut = T.usertime();
 	if (ut < 0.0000000001) ut = 0;
-	return o << T.realtime() << "s (" << ut << " cpu) [" << T.count() << "]"; }
- 
+	return o << T.realtime() << "s (" << ut << " cpu) [" << T.count() << "]";
+}
+
+
+#include <omp.h>
+struct OMPTimer {
+	double _c;
+	void start() { _c = omp_get_wtime(); }
+	void stop() { _c = omp_get_wtime() - _c; }
+	void clear() { _c = 0.0; }
+	double realtime() const { return _c; }
+	double usertime() const { return _c; }
+	OMPTimer& operator =(const OMPTimer& t) { _c = t._c; return *this; }
+	OMPTimer& operator+=(const OMPTimer& t) { _c += t._c; return *this; }
+	OMPTimer& operator-=(const OMPTimer& t) { _c -= t._c; return *this; }
+	OMPTimer  operator +(const OMPTimer& t) const
+	{
+		OMPTimer r; r._c = _c + t._c; return r;
+	}
+	OMPTimer  operator -(const OMPTimer& t) const
+	{
+		OMPTimer r; r._c = _c - t._c; return r;
+	}
+	OMPTimer  operator -() { OMPTimer r; r._c = - _c; return r; }
+};
+//#endif
+//
+inline std::ostream &operator << (std::ostream &o, const OMPTimer &T)
+{
+	return o << T.usertime() << "s" ;
+}
+
 
 
 #include "timer.C"
 
-#endif 
+#endif
