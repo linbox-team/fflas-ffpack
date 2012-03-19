1b0e4c3b9986873829060041a8ae08b36182124f
bboyer <bboyer@ba0639da-81a4-4d54-96fc-8253e07fa939>
2012-03-19 17:48:37 +0000

    unforget this inclusion if stand-alone

--- tests/test-utils.h
+++ tests/test-utils.h
@@ -5,20 +5,20 @@
  * Copyright (C) FFLAS-FFPACK
  * Written by Brice Boyer <bboyer@imag.fr>
  * This file is Free Software and part of FFLAS-FFPACK.
- * 
+ *
  * ========LICENCE========
  * This file is part of the library FFLAS-FFPACK.
- * 
+ *
  * FFLAS-FFPACK is free software: you can redistribute it and/or modify
  * it under the terms of the  GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
@@ -37,6 +37,7 @@
 
 #include "fflas-ffpack/field/nonzero-randiter.h"
 #include "fflas-ffpack/utils/debug.h"
+#include "fflas-ffpack/ffpack/ffpack.h"
 
 namespace FFPACK {
 
