99f7597b79be97bd47ea7cc0460a89c61167b31f
pernet <pernet@ba0639da-81a4-4d54-96fc-8253e07fa939>
2014-06-30 10:41:17 +0000

    new ParSeq trait

--- tests/test-fgemm-long.C
+++ tests/test-fgemm-long.C
@@ -202,7 +202,7 @@ bool launch_MM(const Field & F,
 		}
 		RandomMatrix(F,C,m,n,ldc);
 		FFLAS::fcopy(F,m,n,C,ldc,D,n);
-        FFLAS::MMHelper<Field,FFLAS::MMHelperAlgo::Winograd> WH(F,nbw);
+		FFLAS::MMHelper<Field,FFLAS::MMHelperAlgo::Winograd> WH(F,nbw, FFLAS::ParSeqHelper::Sequential());
 		FFLAS::fgemm (F, ta, tb,m,n,k,alpha, A,lda, B,ldb,
 			      beta,C,ldc,WH);
 		ok &= check_MM(F, D, ta, tb,m,n,k,alpha, A,lda, B,ldb,
