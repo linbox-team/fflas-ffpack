e78dabc78f81c3d771a33c9ced10f8877cb73bb3
giorgi <giorgi@ba0639da-81a4-4d54-96fc-8253e07fa939>
2014-09-08 12:17:37 +0000

    - add new feature to rns-double - remove memory bug with malloc_align

--- fflas-ffpack/utils/align-allocator.h
+++ fflas-ffpack/utils/align-allocator.h
@@ -64,7 +64,8 @@ T* malloc_align(size_t size, Alignment alignment = Alignment::AVX) noexcept
   err = posix_memalign(&p, (size_t) alignment, size*sizeof(T));
   if(err)
     std::cout << "posix_memalign error" << std::endl;
-  return new(p) T[size];
+  //return new(p) T[size];
+  return (T*)p;
 }
 
 namespace detail {
