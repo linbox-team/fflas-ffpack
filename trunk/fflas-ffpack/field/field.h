e983ce82ab5054002b3163bdfa2c939f99381e0e
bboyer <bboyer@ba0639da-81a4-4d54-96fc-8253e07fa939>
2014-09-19 19:39:46 +0000

    oops

---
+++ trunk/fflas-ffpack/field/field.h
@@ -0,0 +1,47 @@
+/* -*- mode: C++; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+// vim:sts=8:sw=8:ts=8:noet:sr:cino=>s,f0,{0,g0,(0,\:0,t0,+0,=s
+
+/*
+ * Copyright (C) FFLAS group
+ *
+ * Written by BB <brice.boyer@lip6.fr>
+ *
+ * ========LICENCE========
+ * This file is part of the library FFLAS-FFPACK.
+ *
+ * FFLAS-FFPACK is free software: you can redistribute it and/or modify
+ * it under the terms of the  GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * ========LICENCE========
+ *
+ */
+
+
+/*! @file field/field.h
+ * @ingroup field
+ * @brief just include them all
+ */
+
+#ifndef __FFLASFFPACK_field_field_H
+#define __FFLASFFPACK_field_field_H
+
+#include "field-general.h"
+#include "modular-balanced.h"
+#include   "modular-positive.h"
+#include   "nonzero-randiter.h"
+#include   "modular-randiter.h"
+#include   "unparametric.h"
+#include   "rns.h"
+
+
+#endif // __FFLASFFPACK_field_field_H
