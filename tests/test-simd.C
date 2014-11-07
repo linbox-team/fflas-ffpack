b52b6580f675bd828c40f62c29fe3fcf501c3d78
vialla <vialla@ba0639da-81a4-4d54-96fc-8253e07fa939>
2014-11-07 10:46:43 +0000

    update test-simd

--- tests/test-simd.C
+++ tests/test-simd.C
@@ -33,110 +33,239 @@
  #include <vector>
  #include <algorithm>
  #include <random>
+ #include <tuple>
+ #include <type_traits>
+ #include <string>
+ #include <iterator>
 
-template<class FuncT, class Element>
-inline void test_binary(FuncT && func, size_t n, const Element * a, const Element * b, const Element * c)
-{
-	using simd = Simd<Element>;
-	typename simd::vect_t v1, v2;
-	for(size_t i = 0 ; i < n ; i+=simd::vect_size){
-		v1 = simd::load(a+i);
-		v2 = simd::load(b+i);
-		simd::store(c+i, func(v1, v2));
-	}
+/**********************************************************************************
+ *
+ * Function Traits
+ *
+ ***********************************************************************************/
+
+template <class F> struct function_traits;
+
+// function pointer
+template <class R, class... Args>
+struct function_traits<R (*)(Args...)> : public function_traits<R(Args...)> {};
+
+template <class R, class... Args> struct function_traits<R(Args...)> {
+  using return_type = R;
+
+  static constexpr std::size_t arity = sizeof...(Args);
+
+  template <std::size_t N> struct argument {
+    static_assert(N < arity, "error: invalid parameter index.");
+    using type = typename std::tuple_element<N, std::tuple<Args...> >::type;
+  };
+};
+
+// member function pointer
+template <class C, class R, class... Args>
+struct function_traits<R (C::*)(Args...)> : public function_traits<
+                                                R(C&, Args...)> {};
+
+// const member function pointer
+template <class C, class R, class... Args>
+struct function_traits<R (C::*)(Args...)
+                       const> : public function_traits<R(C&, Args...)> {};
+
+// member object pointer
+template <class C, class R>
+struct function_traits<R(C::*)> : public function_traits<R(C&)> {};
+
+/**************************************************************************************/
+
+template<class simd, class Element, class SimdFunc, class ScalFunc>
+inline
+typename std::enable_if<
+						(function_traits<SimdFunc>::arity == 1) &&
+						!(std::is_same<typename function_traits<SimdFunc>::return_type, void>::value)
+					   , bool>::type
+test_op(SimdFunc fsimd, ScalFunc fscal, size_t seed, size_t vectorSize, Element max, std::string name){
+	
+	using vect_t = typename simd::vect_t;
+
+	std::mt19937 generator(seed);
+ 	std::uniform_real_distribution<> dist(1, (int)max);
+
+ 	std::vector<Element, AlignedAllocator<Element, Alignment::AVX>> a1(vectorSize), c1(vectorSize), a2(vectorSize), c2(vectorSize);
+ 	std::generate(a1.begin(), a1.end(), [&](){return dist(generator);});
+ 	a2 = a1;
+
+ 	std::transform(a1.begin(), a1.end(), c1.begin(), fscal);
+
+ 	vect_t va2, vc2;
+ 	for(size_t i = 0 ; i < vectorSize ; i+=simd::vect_size){
+ 		va2 = simd::load(a2.data()+i);
+ 		vc2 = fsimd(va2);
+ 		simd::store(c2.data()+i, vc2);
+ 	}
+
+ 	bool res = std::equal(c1.begin(), c1.end(), c2.begin());
+ 	if(!res)
+ 	{
+ 		std::cout << "Error Simd" << sizeof(typename simd::scalar_t)*simd::vect_size*8 << "::" << name << std::endl;
+  		std::copy(c1.begin(), c1.end(), std::ostream_iterator<Element>(std::cout, " "));
+  		std::cout << std::endl;
+  		std::copy(c2.begin(), c2.end(), std::ostream_iterator<Element>(std::cout, " "));
+  		std::cout << std::endl;
+ 	}
+ 	return res;
 }
 
-template<class FuncT, class Element>
-inline void test_unary(FuncT && func, size_t n, const Element * a)
-{
-	using simd = Simd<Element>;
-	typename simd::vect_t v1;
-	for(size_t i = 0 ; i < n ; i+=simd::vect_size){
-		v1 = simd::load(a+i);
-		simd::store(a+i, func(v1));
-	}
+template<class simd, class Element, class SimdFunc, class ScalFunc>
+inline
+typename std::enable_if<
+						(function_traits<SimdFunc>::arity == 0) &&
+						!(std::is_same<typename function_traits<SimdFunc>::return_type, void>::value)
+					   , bool>::type
+test_op(SimdFunc && fsimd, ScalFunc && fscal, size_t seed, size_t vectorSize, Element max, std::string name){
+	
+	using vect_t = typename simd::vect_t;
+
+	std::mt19937 generator(seed);
+ 	std::uniform_real_distribution<Element> dist(1, (int)max);
+
+ 	std::vector<Element, AlignedAllocator<Element, Alignment::AVX>> c1(vectorSize), c2(vectorSize);
+
+ 	std::transform(c1.begin(), c1.end(), c1.begin(), fscal);
+
+ 	vect_t vc2;
+ 	for(size_t i = 0 ; i < vectorSize ; i+=simd::vect_size){
+ 		c2 = fsimd();
+ 		simd::store(c2.data()+i, c2);
+ 	}
+
+ 	bool res = std::equal(c1.begin(), c1.end(), c2.begin());
+ 	if(!res)
+ 	{
+ 		std::cout << "Error Simd" << sizeof(typename simd::scalar_t)*simd::vect_size*8 << "::" << name << std::endl;
+  		std::copy(c1.begin(), c1.end(), std::ostream_iterator<Element>(std::cout, " "));
+  		std::cout << std::endl;
+  		std::copy(c2.begin(), c2.end(), std::ostream_iterator<Element>(std::cout, " "));
+  		std::cout << std::endl;
+ 	}
+ 	return res;
 }
 
-template<class FuncT, class Element>
-inline void test_znary(FuncT && func, size_t n, const Element * a)
-{
-	using simd = Simd<Element>;
-	for(size_t i = 0 ; i < n ; i+=simd::vect_size){
-		simd::store(a+i, func());
-	}
+template<class simd, class Element, class SimdFunc, class ScalFunc>
+inline
+typename std::enable_if<
+						(function_traits<SimdFunc>::arity == 2) &&
+						!(std::is_same<typename function_traits<SimdFunc>::return_type, void>::value)
+					   , bool>::type
+test_op(SimdFunc fsimd, ScalFunc fscal, size_t seed, size_t vectorSize, Element max, std::string name){
+	
+	using vect_t = typename simd::vect_t;
+
+	std::mt19937 generator(seed);
+ 	std::uniform_real_distribution<> dist(1, (int)max);
+
+ 	std::vector<Element, AlignedAllocator<Element, Alignment::AVX>> a1(vectorSize), b1(vectorSize), c1(vectorSize), a2(vectorSize), b2(vectorSize), c2(vectorSize);
+ 	std::generate(a1.begin(), a1.end(), [&](){return dist(generator);});
+ 	std::generate(b1.begin(), b1.end(), [&](){return dist(generator);});
+ 	a2 = a1;
+ 	b2 = b1;
+
+ 	std::transform(a1.begin(), a1.end(), b1.begin(), c1.begin(), fscal);
+
+ 	vect_t va2, vb2, vc2;
+ 	for(size_t i = 0 ; i < vectorSize ; i+=simd::vect_size){
+ 		va2 = simd::load(a2.data()+i);
+ 		vb2 = simd::load(b2.data()+i);
+ 		vc2 = fsimd(va2, vb2);
+ 		simd::store(c2.data()+i, vc2);
+ 	}
+
+ 	bool res = std::equal(c1.begin(), c1.end(), c2.begin());
+ 	if(!res)
+ 	{
+ 		std::cout << "Error Simd" << sizeof(typename simd::scalar_t)*simd::vect_size*8 << "::" << name << std::endl;
+  		std::copy(c1.begin(), c1.end(), std::ostream_iterator<Element>(std::cout, " "));
+  		std::cout << std::endl;
+  		std::copy(c2.begin(), c2.end(), std::ostream_iterator<Element>(std::cout, " "));
+  		std::cout << std::endl;
+ 	}
+ 	return res;
+}
+
+template<class simd, class Element, class SimdFunc, class ScalFunc>
+inline
+typename std::enable_if<
+						(function_traits<SimdFunc>::arity == 3) &&
+						!(std::is_same<typename function_traits<SimdFunc>::return_type, void>::value)
+					   , bool>::type
+test_op(SimdFunc fsimd, ScalFunc fscal, size_t seed, size_t vectorSize, Element max, std::string name){
+	
+	// using simd = SimdT<Element>;
+	using vect_t = typename simd::vect_t;
+
+	std::mt19937 generator(seed);
+ 	std::uniform_real_distribution<> dist(1, (int)max);
+
+ 	std::vector<Element, AlignedAllocator<Element, Alignment::AVX>> a1(vectorSize), b1(vectorSize), c1(vectorSize), d1(vectorSize), a2(vectorSize), b2(vectorSize), c2(vectorSize), d2(vectorSize);
+ 	std::generate(a1.begin(), a1.end(), [&](){return dist(generator);});
+ 	std::generate(b1.begin(), b1.end(), [&](){return dist(generator);});
+ 	std::generate(c1.begin(), c1.end(), [&](){return dist(generator);});
+ 	a2 = a1;
+ 	b2 = b1;
+ 	c2 = c1;
+
+ 	for(size_t i = 0 ; i < vectorSize ; ++i){
+ 		d1[i] = fscal(c1[i], a1[i], b1[i]);
+ 	}
+
+ 	vect_t va2, vb2, vc2;
+ 	for(size_t i = 0 ; i < vectorSize ; i+=simd::vect_size){
+ 		va2 = simd::load(a2.data()+i);
+ 		vb2 = simd::load(b2.data()+i);
+ 		vc2 = simd::load(c2.data()+i);
+ 		simd::store(d2.data()+i, fsimd(vc2, va2, vb2));
+ 	}
+
+ 	bool res = std::equal(d1.begin(), d1.end(), d2.begin());
+ 	if(!res)
+ 	{
+ 		std::cout << "Error Simd" << sizeof(typename simd::scalar_t)*simd::vect_size*8 << "::" << name << std::endl;
+  		std::copy(d1.begin(), d1.end(), std::ostream_iterator<Element>(std::cout, " "));
+  		std::cout << std::endl;
+  		std::copy(d2.begin(), d2.end(), std::ostream_iterator<Element>(std::cout, " "));
+  		std::cout << std::endl;
+ 	}
+ 	return res;
+}
+
+
+template<class simd, class Element>
+bool test_float_impl(size_t seed, size_t vectorSize, Element max){
+	bool btest = true;
+
+	btest &= test_op<simd>(simd::ceil, [](Element x){return std::ceil(x);}, seed, vectorSize, max, "ceil");
+	btest &= test_op<simd>(simd::floor, [](Element x){return std::floor(x);}, seed, vectorSize, max,"floor");
+	btest &= test_op<simd>(simd::round, [](Element x){return std::round(x);}, seed, vectorSize, max, "round");
+	btest &= test_op<simd>(simd::add, [](Element x1, Element x2){return x1+x2;}, seed, vectorSize, max, "add");
+	btest &= test_op<simd>(simd::sub, [](Element x1, Element x2){return x1-x2;}, seed, vectorSize, max, "sub");
+	btest &= test_op<simd>(simd::mul, [](Element x1, Element x2){return x1*x2;}, seed, vectorSize, max, "mul");
+	btest &= test_op<simd>(simd::fmadd, [](Element x1, Element x2, Element x3){return x1+x3*x2;}, seed, vectorSize, max, "fmadd");
+	btest &= test_op<simd>(simd::fmsub, [](Element x1, Element x2, Element x3){return -x1+x3*x2;}, seed, vectorSize, max, "fmsub");
+	btest &= test_op<simd>(simd::fnmadd, [](Element x1, Element x2, Element x3){return x1-x3*x2;}, seed, vectorSize, max, "fnmadd");
+
+	return btest;
 }
 
 template<class Element>
-bool test_float(size_t seed)
-{
- 	using simd = Simd<Element>;
-
- 	std::mt19937 generator(seed);
- 	std::uniform_real_distribution<> dist(1, 100);
-
- 	size_t vectorSize = 32;
- 	std::vector<Element, AlignedAllocator<Element, Alignment::AVX>> a1(vectorSize), b1(vectorSize), c1(vectorSize), a2(vectorSize), b2(vectorSize), c2(vectorSize);
-
- 	test_znary(simd::zero, vectorSize, a2.data());
-	std::fill(a1.begin(), a1.end(), 0);
-	if(!std::equal(a1.begin(), a1.end(), a2.begin())){
-		return false;
-	}
-
-	std::generate(a1.begin(), a1.end(), [&](){return dist(generator);});
-	std::generate(b1.begin(), b1.end(), [&](){return dist(generator);});
-	a2 = a1;
-	b2 = b1;
-
-	test_binary(simd::add, vectorSize, a2.data(), b2.data(), c2.data());
-	for(size_t i = 0 ; i < vectorSize ; ++i)
-		c1[i] = b1[i]+a1[i];
-	if(!std::equal(c1.begin(), c1.end(), c2.begin())){
-		return false;
-	}
-
-	std::fill(a1.begin(), a1.end(), 0);
-	std::fill(a2.begin(), a2.end(), 0);
-	std::fill(b1.begin(), b1.end(), 0);
-	std::fill(b2.begin(), b2.end(), 0);
-
-	std::generate(a1.begin(), a1.end(), [&](){return dist(generator);});
-	std::generate(b1.begin(), b1.end(), [&](){return dist(generator);});
-	a2 = a1;
-	b2 = b1;
-
-	test_binary(simd::mul, vectorSize, a2.data(), b2.data(), c2.data());
-	for(size_t i = 0 ; i < vectorSize ; ++i)
-		c1[i] = b1[i]*a1[i];
-	if(!std::equal(c1.begin(), c1.end(), c2.begin())){
-		return false;
-	}
-
-	std::fill(a1.begin(), a1.end(), 0);
-	std::fill(a2.begin(), a2.end(), 0);
-	std::fill(b1.begin(), b1.end(), 0);
-	std::fill(b2.begin(), b2.end(), 0);
-
-	std::generate(a1.begin(), a1.end(), [&](){return dist(generator);});
-	std::generate(b1.begin(), b1.end(), [&](){return dist(generator);});
-	a2 = a1;
-	b2 = b1;
-
-	test_binary(simd::sub, vectorSize, a2.data(), b2.data(), c2.data());
-	for(size_t i = 0 ; i < vectorSize ; ++i)
-		c1[i] = a1[i]-b1[i];
-	if(!std::equal(c1.begin(), c1.end(), c2.begin())){
-		return false;
-	}
-
-	std::fill(a1.begin(), a1.end(), 0);
-	std::fill(a2.begin(), a2.end(), 0);
-	std::fill(b1.begin(), b1.end(), 0);
-	std::fill(b2.begin(), b2.end(), 0);
-
-	return true;
+bool test_float(size_t seed, size_t vectorSize, size_t max_){
+	bool sse = true, avx = true;
+	sse = test_float_impl<Simd128<Element>>(seed, vectorSize, (Element)max_);
+	if(!sse)
+		std::cout << "bug sse" << std::endl;
+	avx = test_float_impl<Simd256<Element>>(seed, vectorSize, (Element)max_);
+	if(!avx)
+		std::cout << "bug avx" << std::endl;
+	return sse && avx;
 }
-
 
  template<class Element>
  bool test_integer()
@@ -147,26 +276,26 @@ bool test_float(size_t seed)
 
  int main(int ac, char **av) {
 	int seed = (int) time(NULL);
+	int vectorSize = 32;
+	int max = 100;
 
 	static Argument as[] = {
 		{ 's', "-s N", "Set the seed                 .", TYPE_INT , &seed },
 		END_OF_ARGUMENTS
 	};
 
-
 	FFLAS::parseArguments(ac,av,as);
 
 	srand(seed);
 	srand48(seed);
 
-
 	bool pass  = true ;
-	{ /*  finit */
+	{ 
 		{
-			pass &= test_float<float>(seed);
+			pass &= test_float<float>(seed, vectorSize, max);
 		}
 		{
-			pass &= test_float<double>(seed);
+			pass &= test_float<double>(seed, vectorSize, max);
 		}
 	}
 	std::cout << std::boolalpha << pass << std::endl;
