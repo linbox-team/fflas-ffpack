30dfb7cbf6a59d04bc534b57f51fb25b33eec506
dumas <dumas@ba0639da-81a4-4d54-96fc-8253e07fa939>
2014-12-03 16:05:35 +0000

    Integer --> Field::FieldInt

--- trunk/fflas-ffpack/utils/Matio.h
+++ trunk/fflas-ffpack/utils/Matio.h
@@ -238,7 +238,7 @@ std::ostream& write_field(const Field& F,std::ostream& c,
 			  int n, int m, int id, bool mapleFormat = false, bool column_major=false)
 {
 
-	FFPACK::Integer tmp;
+	typename Field::FieldInt tmp;
 	// double tmp;
 	if (mapleFormat) c << "Matrix(" << n <<',' << m << ", [" ;
 	for (int i = 0; i<n;++i){
