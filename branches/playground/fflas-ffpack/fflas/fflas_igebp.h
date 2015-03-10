962c13b12bcb7118153da9f283a6a674083e6fd9
bboyer <bboyer@ba0639da-81a4-4d54-96fc-8253e07fa939>
2015-03-10 18:18:37 +0000

    remove fflas_fspmv (to attic)

--- branches/playground/fflas-ffpack/fflas/fflas_igebp.h
+++ branches/playground/fflas-ffpack/fflas/fflas_igebp.h
@@ -25,10 +25,10 @@
  *.
  */
 
-#ifdef __AVX2__ 
+#ifdef __AVX2__
 #define _vect_t __m256i
 #define _vect_align 32
-#define _vect_s 4 
+#define _vect_s 4
 #define _nr 4
 #define _mr 8
 #define StepA 4
@@ -43,7 +43,7 @@
 #else
 #define _vect_t __m128i
 #define _vect_align 16
-#define _vect_s 2 
+#define _vect_s 2
 #define _nr 4
 #define _mr 4
 #define StepA 2
@@ -61,7 +61,7 @@
 
 
 // I ASSUME _vect_s <= 4 (meaning that we store at most 4 element per vector register)
-// -> this is to explicitely treat the dimension that are not divisible by _vect_s 
+// -> this is to explicitely treat the dimension that are not divisible by _vect_s
 
 
 inline void igebb44(size_t i, size_t j, size_t depth, size_t pdeth, int64_t* C, size_t ldc, const int64_t *blA, const int64_t* blB);
@@ -73,7 +73,7 @@ inline void igebb11(size_t i, size_t j, size_t depth, size_t pdeth, int64_t* C, 
 
 void prefetch(const int64_t* addr) { _mm_prefetch((const char*)(addr), _MM_HINT_T0); }
 
-// duplicate each entry into vector register 
+// duplicate each entry into vector register
 template<size_t N>
 inline void duplicate_vect (int64_t* XX, const int64_t* X, size_t n){}
 
@@ -86,6 +86,7 @@ inline void duplicate_vect<2>(int64_t* XX, const int64_t* X, size_t n){
 		p+=2;
 	}
 }
+
 template<>
 inline void duplicate_vect<4>(int64_t* XX, const int64_t* X, size_t n){
 	int64_t *p=XX;
@@ -103,9 +104,9 @@ inline void duplicate_vect<4>(int64_t* XX, const int64_t* X, size_t n){
  *  MAIN GEBP OPERATION  *
  ************************/
 
-void igebp( size_t rows, size_t cols, size_t depth, int64_t* C, size_t ldc, const int64_t* blockA, size_t lda, 
-	    const int64_t* blockB, size_t ldb, int64_t* blockW){  
-	
+void igebp( size_t rows, size_t cols, size_t depth, int64_t* C, size_t ldc, const int64_t* blockA, size_t lda,
+	    const int64_t* blockB, size_t ldb, int64_t* blockW){
+
 	size_t i,j,k;
 	size_t prows,pcols,pdepth;
 	prows=(rows/_mr)*_mr;
@@ -118,7 +119,7 @@ void igebp( size_t rows, size_t cols, size_t depth, int64_t* C, size_t ldc, cons
 		// process rows by pack of _mr
 		for (i=0;i<prows;i+=_mr){
 			const int64_t* blA = blockA+i*lda;
-			prefetch(blA); 
+			prefetch(blA);
 			igebb44(i, j, depth, pdepth, C, ldc, blA, blockW);
 		}
 		i=prows;
@@ -126,15 +127,15 @@ void igebp( size_t rows, size_t cols, size_t depth, int64_t* C, size_t ldc, cons
 		int rem=rows-prows;
 		while (rem >0) {
 			if (rem>=_vect_s){
-				igebb24(i  ,j,depth, pdepth, C, ldc, blockA+i*lda, blockW); 
-				i+=_vect_s;	
+				igebb24(i  ,j,depth, pdepth, C, ldc, blockA+i*lda, blockW);
+				i+=_vect_s;
 				rem-=_vect_s;
 			}
-			else{	// use blockB since no vectorization						
-				igebb14(i,j,depth, pdepth, C, ldc, blockA+i*lda, blockB+j*ldb); 
+			else{	// use blockB since no vectorization
+				igebb14(i,j,depth, pdepth, C, ldc, blockA+i*lda, blockB+j*ldb);
 				i++;
 				rem--;
-			}					
+			}
 		}
 	}
 	// process the (columns%_nr) remaining columns one by one
@@ -150,18 +151,18 @@ void igebp( size_t rows, size_t cols, size_t depth, int64_t* C, size_t ldc, cons
 		i=prows;
 		// process the (rows%_mr) remainings rows
 		int rem=rows-prows;
-		while (rem >0) {			
+		while (rem >0) {
 			if (rem>=_vect_s){
-				igebb21(i  ,j,depth, pdepth, C, ldc, blockA+i*lda, blockW); 
-				i+=_vect_s;	
+				igebb21(i  ,j,depth, pdepth, C, ldc, blockA+i*lda, blockW);
+				i+=_vect_s;
 				rem-=_vect_s;
 			}
-			else{   // use blockB since no vectorization			
-				igebb11(i,j,depth, pdepth, C, ldc, blockA+i*lda, blockB+j*ldb); 
+			else{   // use blockB since no vectorization
+				igebb11(i,j,depth, pdepth, C, ldc, blockA+i*lda, blockB+j*ldb);
 				i++;
 				rem--;
-			}		
-		}		
+			}
+		}
 	}
 }
 
@@ -182,7 +183,7 @@ inline void igebb44(size_t i, size_t j, size_t depth, size_t pdepth, int64_t* C,
 	VEC_SET_64(C6,0L);
 	VEC_SET_64(C7,0L);
 	int64_t *r0 = C+j*ldc+i;
-	int64_t *r1 = r0+ldc; 
+	int64_t *r1 = r0+ldc;
 	int64_t *r2 = r1+ldc;
 	int64_t *r3 = r2+ldc;
 	prefetch(r0+_vect_s);
@@ -190,22 +191,22 @@ inline void igebb44(size_t i, size_t j, size_t depth, size_t pdepth, int64_t* C,
 	prefetch(r2+_vect_s);
 	prefetch(r3+_vect_s);
 	// process the loop by (_mrx4) by (4x4) matrix mul
-	for (k=0;k<pdepth;k+=4){ 				
+	for (k=0;k<pdepth;k+=4){
 		_vect_t A0,A1;
-		_vect_t B0,B1,B2,B3; 
-		_vect_t Tmp;				
+		_vect_t B0,B1,B2,B3;
+		_vect_t Tmp;
 		VEC_LOAD(A0, blA+0*StepA);
 		VEC_LOAD(A1, blA+1*StepA);
 		VEC_LOAD(B0, blB+0*StepB);
-		VEC_LOAD(B1, blB+1*StepB); 
+		VEC_LOAD(B1, blB+1*StepB);
 		VEC_MADD_32(C0,A0,B0,Tmp);
 		VEC_LOAD(B2, blB+2*StepB);
 		VEC_MADD_32(C4,A1,B0,B0);
-		VEC_LOAD(B3, blB+3*StepB);			 
+		VEC_LOAD(B3, blB+3*StepB);
 		VEC_LOAD(B0, blB+4*StepB);
 		VEC_MADD_32(C1,A0,B1,Tmp);
-		VEC_MADD_32(C5,A1,B1,B1);			
-		VEC_LOAD(B1, blB+5*StepB);		  
+		VEC_MADD_32(C5,A1,B1,B1);
+		VEC_LOAD(B1, blB+5*StepB);
 		VEC_MADD_32(C2,A0,B2,Tmp);
 		VEC_MADD_32(C6,A1,B2,B2);
 		VEC_LOAD(B2, blB+6*StepB);
@@ -218,12 +219,12 @@ inline void igebb44(size_t i, size_t j, size_t depth, size_t pdepth, int64_t* C,
 		VEC_MADD_32(C4,A1,B0,B0);
 		VEC_LOAD(B0, blB+8*StepB);
 		VEC_MADD_32(C1,A0,B1,Tmp);
-		VEC_MADD_32(C5,A1,B1,B1);	  		
+		VEC_MADD_32(C5,A1,B1,B1);
 		VEC_LOAD(B1, blB+9*StepB);
 		VEC_MADD_32(C2,A0,B2,Tmp);
 		VEC_MADD_32(C6,A1,B2,B2);
 		VEC_LOAD(B2, blB+10*StepB);
-		VEC_MADD_32(C3,A0,B3,Tmp); 
+		VEC_MADD_32(C3,A0,B3,Tmp);
 		VEC_LOAD(A0, blA+4*StepA);
 		VEC_MADD_32(C7,A1,B3,B3);
 		VEC_LOAD(A1, blA+5*StepA);
@@ -232,7 +233,7 @@ inline void igebb44(size_t i, size_t j, size_t depth, size_t pdepth, int64_t* C,
 		VEC_MADD_32(C4,A1,B0,B0);
 		VEC_LOAD(B0, blB+12*StepB);
 		VEC_MADD_32(C1,A0,B1,Tmp);
-		VEC_MADD_32(C5,A1,B1,B1);				
+		VEC_MADD_32(C5,A1,B1,B1);
 		VEC_LOAD(B1, blB+13*StepB);
 		VEC_MADD_32(C2,A0,B2,Tmp);
 		VEC_MADD_32(C6,A1,B2,B2);
@@ -249,19 +250,19 @@ inline void igebb44(size_t i, size_t j, size_t depth, size_t pdepth, int64_t* C,
 		VEC_MADD_32(C2,A0,B2,Tmp);
 		VEC_MADD_32(C6,A1,B2,B2);
 		VEC_MADD_32(C3,A0,B3,Tmp);
-		VEC_MADD_32(C7,A1,B3,B3);									
+		VEC_MADD_32(C7,A1,B3,B3);
 		blA+= 8*StepA;
 		blB+=16*StepB;
 	}
 	// process (depth mod 4) remaining entries by  (_mrx1) by (1x4) matrix mul
-	for(;k<depth;k++){ 
+	for(;k<depth;k++){
 		_vect_t A0,A1;
-		_vect_t B0,B1,B2,B3; 
-		_vect_t Tmp;				
+		_vect_t B0,B1,B2,B3;
+		_vect_t Tmp;
 		VEC_LOAD(A0, blA+0*StepA);
 		VEC_LOAD(A1, blA+1*StepA);
 		VEC_LOAD(B0, blB+0*StepB);
-		VEC_LOAD(B1, blB+1*StepB); 				
+		VEC_LOAD(B1, blB+1*StepB);
 		VEC_MADD_32(C0,A0,B0,Tmp);
 		VEC_LOAD(B2, blB+2*StepB);
 		VEC_MADD_32(C4,A1,B0,B0);
@@ -271,26 +272,26 @@ inline void igebb44(size_t i, size_t j, size_t depth, size_t pdepth, int64_t* C,
 		VEC_MADD_32(C2,A0,B2,Tmp);
 		VEC_MADD_32(C6,A1,B2,B2);
 		VEC_MADD_32(C3,A0,B3,Tmp);
-		VEC_MADD_32(C7,A1,B3,B3);	
+		VEC_MADD_32(C7,A1,B3,B3);
 		blA+=2*StepA;
 		blB+=4*StepB;
 	}
-	_vect_t R0, R1, R2, R3, R4, R5, R6;			
+	_vect_t R0, R1, R2, R3, R4, R5, R6;
 	VEC_LOADU(R0, r0);
 	VEC_LOADU(R1, r1);
 	VEC_LOADU(R2, r2);
 	VEC_LOADU(R3, r3);
 	VEC_LOADU(R4, r0+_vect_s);
 	VEC_LOADU(R5, r1+_vect_s);
-	VEC_LOADU(R6, r2+_vect_s);			
-	VEC_ADDIN_64(R0,C0);			
+	VEC_LOADU(R6, r2+_vect_s);
+	VEC_ADDIN_64(R0,C0);
 	VEC_STOREU(r0,R0);
 	VEC_LOADU(R0, r3+_vect_s);
 	VEC_ADDIN_64(R1,C1);
 	VEC_ADDIN_64(R2,C2);
 	VEC_ADDIN_64(R3,C3);
 	VEC_ADDIN_64(R4,C4);
-	VEC_ADDIN_64(R5,C5); 
+	VEC_ADDIN_64(R5,C5);
 	VEC_ADDIN_64(R6,C6);
 	VEC_ADDIN_64(R0,C7);
 	VEC_STOREU(r1,R1);
@@ -299,8 +300,8 @@ inline void igebb44(size_t i, size_t j, size_t depth, size_t pdepth, int64_t* C,
 	VEC_STOREU(r0+_vect_s,R4);
 	VEC_STOREU(r1+_vect_s,R5);
 	VEC_STOREU(r2+_vect_s,R6);
-	VEC_STOREU(r3+_vect_s,R0);					
-  
+	VEC_STOREU(r3+_vect_s,R0);
+
 }
 
 
@@ -313,35 +314,35 @@ inline void igebb24(size_t i, size_t j, size_t depth, size_t pdepth, int64_t* C,
 	VEC_SET_64(C2,0L);
 	VEC_SET_64(C3,0L);
 	int64_t *r0 = C+j*ldc+i;
-	int64_t *r1 = r0+ldc; 
+	int64_t *r1 = r0+ldc;
 	int64_t *r2 = r1+ldc;
 	int64_t *r3 = r2+ldc;
 	// process the loop by (1/2_mrx4) by (4x4) matrix mul
-	for (k=0;k<pdepth;k+=4){ 				
+	for (k=0;k<pdepth;k+=4){
 		_vect_t A0;
-		_vect_t B0,B1,B2,B3; 
-		_vect_t Tmp;	
+		_vect_t B0,B1,B2,B3;
+		_vect_t Tmp;
 		VEC_LOAD(A0, blA+0*StepA);
 		VEC_LOAD(B0, blB+0*StepB);
-		VEC_LOAD(B1, blB+1*StepB); 
+		VEC_LOAD(B1, blB+1*StepB);
 		VEC_MADD_32(C0,A0,B0,Tmp);
 		VEC_LOAD(B2, blB+2*StepB);
-		VEC_LOAD(B3, blB+3*StepB);			 
+		VEC_LOAD(B3, blB+3*StepB);
 		VEC_LOAD(B0, blB+4*StepB);
 		VEC_MADD_32(C1,A0,B1,Tmp);
-		VEC_LOAD(B1, blB+5*StepB);		  
+		VEC_LOAD(B1, blB+5*StepB);
 		VEC_MADD_32(C2,A0,B2,Tmp);
 		VEC_LOAD(B2, blB+6*StepB);
 		VEC_MADD_32(C3,A0,B3,Tmp);
 		VEC_LOAD(A0, blA+1*StepA);
 		VEC_LOAD(B3, blB+7*StepB);
 		VEC_MADD_32(C0,A0,B0,Tmp);
-		VEC_LOAD(B0, blB+8*StepB); 
+		VEC_LOAD(B0, blB+8*StepB);
 		VEC_MADD_32(C1,A0,B1,Tmp);
 		VEC_LOAD(B1, blB+9*StepB);
 		VEC_MADD_32(C2,A0,B2,Tmp);
 		VEC_LOAD(B2, blB+10*StepB);
-		VEC_MADD_32(C3,A0,B3,Tmp); 
+		VEC_MADD_32(C3,A0,B3,Tmp);
 		VEC_LOAD(A0, blA+2*StepA);
 		VEC_LOAD(B3, blB+11*StepB);
 		VEC_MADD_32(C0,A0,B0,Tmp);
@@ -361,13 +362,13 @@ inline void igebb24(size_t i, size_t j, size_t depth, size_t pdepth, int64_t* C,
 		blB+=16*StepB;
 	}
 	// process (depth mod 4) remaining entries by  (1/2_mrx1) by (1x4) matrix mul
-	for(;k<depth;k++){ 
+	for(;k<depth;k++){
 		_vect_t A0;
-		_vect_t B0,B1,B2,B3; 
-		_vect_t Tmp;				
+		_vect_t B0,B1,B2,B3;
+		_vect_t Tmp;
 		VEC_LOAD(A0, blA+0*StepA);
 		VEC_LOAD(B0, blB+0*StepB);
-		VEC_LOAD(B1, blB+1*StepB); 				
+		VEC_LOAD(B1, blB+1*StepB);
 		VEC_MADD_32(C0,A0,B0,Tmp);
 		VEC_LOAD(B2, blB+2*StepB);
 		VEC_LOAD(B3, blB+3*StepB);
@@ -382,7 +383,7 @@ inline void igebb24(size_t i, size_t j, size_t depth, size_t pdepth, int64_t* C,
 	VEC_LOADU(R1, r1);
 	VEC_LOADU(R2, r2);
 	VEC_LOADU(R3, r3);
-	VEC_ADDIN_64(R0,C0);			
+	VEC_ADDIN_64(R0,C0);
 	VEC_ADDIN_64(R1,C1);
 	VEC_ADDIN_64(R2,C2);
 	VEC_ADDIN_64(R3,C3);
@@ -397,14 +398,14 @@ inline void igebb24(size_t i, size_t j, size_t depth, size_t pdepth, int64_t* C,
 inline void igebb14(size_t i, size_t j, size_t depth, size_t pdepth, int64_t* C, size_t ldc, const int64_t *blA, const int64_t* blB){
 	size_t k;
 	int64_t *r0 = C+j*ldc+i;
-	int64_t *r1 = r0+ldc; 
+	int64_t *r1 = r0+ldc;
 	int64_t *r2 = r1+ldc;
-	int64_t *r3 = r2+ldc;	
-	for(k=0;k<depth;k++){ 
+	int64_t *r3 = r2+ldc;
+	for(k=0;k<depth;k++){
 		r0[0]+=blA[0]*blB[0];
 		r1[0]+=blA[0]*blB[1];
 		r2[0]+=blA[0]*blB[2];
-		r3[0]+=blA[0]*blB[3]; 
+		r3[0]+=blA[0]*blB[3];
 		blA++;
 		blB+=4;
 	}
@@ -417,13 +418,13 @@ inline void igebb41(size_t i, size_t j, size_t depth, size_t pdepth, int64_t* C,
 	VEC_SET_64(C0,0L);
 	VEC_SET_64(C4,0L);
 	int64_t *r0 = C+j*ldc+i;
-	int64_t *r4 = r0+_vect_s; 
+	int64_t *r4 = r0+_vect_s;
 
 	// process the loop by (_mrx1) by (1x1) matrix mul
-	for (k=0;k<depth;k++){ 	
+	for (k=0;k<depth;k++){
 		_vect_t A0,A1;
 		_vect_t B0;
-		_vect_t Tmp;				
+		_vect_t Tmp;
 		VEC_LOAD(A0, blA+0*StepA);
 		VEC_LOAD(A1, blA+1*StepA);
 		VEC_LOAD(B0, blB+0*StepB);
@@ -432,10 +433,10 @@ inline void igebb41(size_t i, size_t j, size_t depth, size_t pdepth, int64_t* C,
 		blA+= 2*StepA;
 		blB+= 1*StepB;
 	}
-	_vect_t R0, R4;			
+	_vect_t R0, R4;
 	VEC_LOADU(R0, r0);
 	VEC_LOADU(R4, r4);
-	VEC_ADDIN_64(R0,C0);			
+	VEC_ADDIN_64(R0,C0);
 	VEC_ADDIN_64(R4,C4);
 	VEC_STOREU(r0,R0);
 	VEC_STOREU(r4,R4);
@@ -449,19 +450,19 @@ inline void igebb21(size_t i, size_t j, size_t depth, size_t pdepth, int64_t* C,
 	int64_t *r0 = C+j*ldc+i;
 
 	// process the loop by (1/2_mrx1) by (1x1) matrix mul
-	for (k=0;k<depth;k++){ 				
+	for (k=0;k<depth;k++){
 		_vect_t A0;
 		_vect_t B0;
-		_vect_t Tmp;				
+		_vect_t Tmp;
 		VEC_LOAD(A0, blA+0*StepA);
 		VEC_LOAD(B0, blB+0*StepB);
 		VEC_MADD_32(C0,A0,B0,Tmp);
 		blA+= 1*StepA;
 		blB+= 1*StepB;
 	}
-	_vect_t R0;			
+	_vect_t R0;
 	VEC_LOADU(R0, r0);
-	VEC_ADDIN_64(R0,C0);			
+	VEC_ADDIN_64(R0,C0);
 	VEC_STOREU(r0,R0);
 }
 
@@ -469,7 +470,7 @@ inline void igebb21(size_t i, size_t j, size_t depth, size_t pdepth, int64_t* C,
 inline void igebb11(size_t i, size_t j, size_t depth, size_t pdepth, int64_t* C, size_t ldc, const int64_t *blA, const int64_t* blB){
 	size_t k;
 	int64_t *r0 = C+j*ldc+i;
-	for(k=0;k<depth;k++){ 
+	for(k=0;k<depth;k++){
 		r0[0]+=blA[k]*blB[k];
 	}
 }
@@ -481,7 +482,7 @@ ostream& operator<<(ostream &os, _vect_t& x){
 	VEC_STORE(T,x);
 	os <<"[";
 	for(size_t i=0;i<_vect_s-1;i++)
-		os<<T[i]<<" ";	
+		os<<T[i]<<" ";
 	os<<T[_vect_s-1]<<"]";
 	return os;
 }
--- branches/playground/fflas-ffpack/fflas/fflas_igemm.h
+++ branches/playground/fflas-ffpack/fflas/fflas_igemm.h
@@ -26,7 +26,7 @@
  */
 
 #include <immintrin.h>
-#include <stdlib.h> 
+#include <stdlib.h>
 
 #include "fflas-ffpack/fflas/fflas_igebp.h"
 
@@ -53,7 +53,7 @@ void igemm_colmajor(size_t rows, size_t cols, size_t depth, int64_t* C, size_t l
 	BlockingFactor(mc,nc,kc);
 	size_t sizeA = mc*kc;
 	size_t sizeB = kc*cols;
-	size_t sizeW = _vect_s*kc*_nr; // store data duplicated by the number of elements fitting in vector register 
+	size_t sizeW = _vect_s*kc*_nr; // store data duplicated by the number of elements fitting in vector register
 
 	// these data must be (_vect_align) byte aligned
 	int64_t *blockA, *blockB, *blockW;
@@ -61,36 +61,36 @@ void igemm_colmajor(size_t rows, size_t cols, size_t depth, int64_t* C, size_t l
 	posix_memalign(reinterpret_cast<void**>(&blockB),_vect_align, sizeof(int64_t)*sizeB);;
 	posix_memalign(reinterpret_cast<void**>(&blockW),_vect_align, sizeof(int64_t)*sizeW);;
 
-			
+
 	// For each horizontal panel of B, and corresponding vertical panel of A
 	for(size_t k2=0; k2<depth; k2+=kc){
-		
+
 		const size_t actual_kc = std::min(k2+kc,depth)-k2;
-		
-		// pack horizontal panel of B into sequential memory (L2 cache)		
+
+		// pack horizontal panel of B into sequential memory (L2 cache)
 		pack_rhs<_nr>(blockB, B+k2, ldc, actual_kc, cols);
-		
+
 		// For each mc x kc block of the lhs's vertical panel...
 		for(size_t i2=0; i2<rows; i2+=mc){
 
 			const size_t actual_mc = std::min(i2+mc,rows)-i2;
-			
+
 			//cout<<"mc= "<<actual_mc<<" kc= "<<actual_mc<<endl;
 
 			// pack a chunk of the vertical panel of A into a sequential memory (L1 cache)
 			pack_lhs<_mr>(blockA, A+i2+k2*lda, lda, actual_mc, actual_kc);
 			// call block*panel kernel
-			igebp(actual_mc, cols, actual_kc, C+i2, ldc, blockA, actual_kc, blockB, actual_kc, blockW);	       
+			igebp(actual_mc, cols, actual_kc, C+i2, ldc, blockA, actual_kc, blockB, actual_kc, blockW);
 		}
 	}
 	free(blockA);
 	free(blockB);
-	free(blockW); 
+	free(blockW);
 }
 
 
-#define SSE_LOADU(A,B)  A=_mm_loadu_si128(reinterpret_cast<const __m128i*>(&B)); 
-#define SSE_STORE(A,B)    _mm_store_si128(reinterpret_cast<__m128i*>(&A),B); 
+#define SSE_LOADU(A,B)  A=_mm_loadu_si128(reinterpret_cast<const __m128i*>(&B));
+#define SSE_STORE(A,B)    _mm_store_si128(reinterpret_cast<__m128i*>(&A),B);
 
 
 // store each rows x k submatrices of Rhs in row major mode
@@ -108,30 +108,30 @@ void pack_rhs(int64_t* XX, const int64_t* X, size_t ldx, size_t rows, size_t col
 	// the remaining columns are not packed
 	for(size_t j=cols_by_k;j<cols;j++)
 		for(size_t i=0;i<rows;i++,p++)
-			XX[p]=X[i+j*ldx]; 
-}	
+			XX[p]=X[i+j*ldx];
+}
 
 
 // store each k x cols submatrices of Lhs in column major mode
 // if k does not divide rows, the remaining rows are not packed
 template<size_t k>
-void pack_lhs(int64_t* XX, const int64_t* X, size_t ldx, size_t rows, size_t cols){	
+void pack_lhs(int64_t* XX, const int64_t* X, size_t ldx, size_t rows, size_t cols){
 	size_t p=0;
 	size_t rows_by_k=(rows/k)*k;
-	// pack rows by group of k 
+	// pack rows by group of k
 	for(size_t i=0;i<rows_by_k;i+=k)
-		for(size_t j=0;j<cols;j++)	
+		for(size_t j=0;j<cols;j++)
 			//for (size_t l=0;l<k;l++,p++) XX[p]=X[i+l+j*ldx];
 				for (size_t l=0;l<k;l+=4){
 				__m128i T0,T1;
 				SSE_LOADU(T0,X[i+l+  j*ldx]);
 				SSE_LOADU(T1,X[i+l+2+j*ldx]);
 				SSE_STORE(XX[p],T0);p+=2;
-				SSE_STORE(XX[p],T1);p+=2;			 
-			}	
+				SSE_STORE(XX[p],T1);p+=2;
+			}
 	// the remaining rows are packed by group of StepA (if possible)
 	if (rows-rows_by_k>=StepA){
-		for(size_t j=0;j<cols;j++)	
+		for(size_t j=0;j<cols;j++)
 			for (size_t l=0;l<StepA;l+=2){
 				__m128i T0;
 				SSE_LOADU(T0,X[rows_by_k+l+j*ldx]);
@@ -142,8 +142,8 @@ void pack_lhs(int64_t* XX, const int64_t* X, size_t ldx, size_t rows, size_t col
 	for(size_t i=rows_by_k;i<rows;i++)
 		for(size_t j=0;j<cols;j++,p++){
 			XX[p]=X[i+j*ldx];
-		} 
-	
+		}
+
 }
 
 #define __CPUID(abcd,func,id) \
@@ -165,7 +165,7 @@ inline void getCacheSize(int& l1, int& l2, int& l3)
 				int ways        = (abcd[1] & 0xFFC00000) >> 22; // B[31:22]
 				int partitions  = (abcd[1] & 0x003FF000) >> 12; // B[21:12]
 				int line_size   = (abcd[1] & 0x00000FFF) >>  0; // B[11:0]
-				int sets        = (abcd[2]);                    // C[31:0]				
+				int sets        = (abcd[2]);                    // C[31:0]
 				int cache_size = (ways+1) * (partitions+1) * (line_size+1) * (sets+1);
 				switch(cache_level)
 					{
@@ -178,7 +178,7 @@ inline void getCacheSize(int& l1, int& l2, int& l3)
 		cache_id++;
 	} while(cache_type>0 && cache_id<16);
 }
- 
+
 
 inline void getTLBSize(int& tlb)
 {
@@ -188,30 +188,30 @@ inline void getTLBSize(int& tlb)
 	unsigned char * bytes = reinterpret_cast<unsigned char *>(abcd)+2;
 	for(int i=0; i<14; ++i)
 		switch(bytes[i]){
-		case 0x03: sTLB=64; break; 
-		case 0x04: lTLB=8;  break; 
-		case 0x05: lTLB=32; break; 
-		case 0x56: lTLB=16; break; 
-		case 0x57: sTLB=16; break; 
-		case 0x59: sTLB=16; break; 
-		case 0x5A: lTLB=32; break; 
-		case 0x5B: sTLB=lTLB=64;  break; 
-		case 0x5C: sTLB=lTLB=128; break; 
-		case 0x5D: sTLB=lTLB=256; break; 
-		case 0xB3: sTLB=128; break; 
-		case 0xB4: sTLB=256; break; 
-		case 0xBA: sTLB=64;  break; 
-		case 0xC0: sTLB=lTLB=8; break; 
-		case 0xCA: sTLB=512; break; 
+		case 0x03: sTLB=64; break;
+		case 0x04: lTLB=8;  break;
+		case 0x05: lTLB=32; break;
+		case 0x56: lTLB=16; break;
+		case 0x57: sTLB=16; break;
+		case 0x59: sTLB=16; break;
+		case 0x5A: lTLB=32; break;
+		case 0x5B: sTLB=lTLB=64;  break;
+		case 0x5C: sTLB=lTLB=128; break;
+		case 0x5D: sTLB=lTLB=256; break;
+		case 0xB3: sTLB=128; break;
+		case 0xB4: sTLB=256; break;
+		case 0xBA: sTLB=64;  break;
+		case 0xC0: sTLB=lTLB=8; break;
+		case 0xCA: sTLB=512; break;
 		default: break;
 		}
 	//cout<<"small TLB: "<<sTLB<<endl;
-	//cout<<"large TLB: "<<lTLB<<endl;			
+	//cout<<"large TLB: "<<lTLB<<endl;
 	tlb=sTLB*4096;
 }
 
 void BlockingFactor(size_t& m, size_t& n, size_t& k)
-{ 
+{
 	int l1, l2, l3, tlb;
 	getCacheSize(l1,l2,l3);
 	getTLBSize(tlb);
@@ -223,14 +223,14 @@ void BlockingFactor(size_t& m, size_t& n, size_t& k)
 	cout<<"TLB ("<<tlb<<") ";
 	cout<<endl;
 	*/
-	l2=std::max(l2,l3);	  
+	l2=std::max(l2,l3);
 	l2=std::min(l2,tlb);
 	size_t kc,mc;
 	// kc * 2*(_mr+_nr) must fit in L1 cache
 	// kc * (n+mc) must fit in L2 cache and in TLB
 	size_t kdiv= 2*(_nr+_mr)*sizeof(int64_t);
-	kc = std::min(k, l1/kdiv); 
-	mc = std::min(m, l2/(sizeof(int64_t) * kc)); 
+	kc = std::min(k, l1/kdiv);
+	mc = std::min(m, l2/(sizeof(int64_t) * kc));
 	k=kc;
        	m=mc;
 	//cout<<"mc="<<m<<endl;
