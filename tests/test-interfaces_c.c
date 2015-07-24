beafb7e4a0f745ef40821f01dfcbbc7285e671fa
Brice Boyer <brice.boyer@lip6.fr>
2015-07-24 13:26:42 +0200

    fix up interface

--- tests/test-interfaces_c.c
+++ tests/test-interfaces_c.c
@@ -4,11 +4,11 @@
 #include <stdio.h>
 
 int main() {
-	double * A = malloc(4*sizeof(double));
+	double * A = (double*)malloc(4*sizeof(double));
 	A[0] = A[2] = 1 ;
 	A[1] = A[3] = 0 ;
-	size_t * P = malloc(2*sizeof(size_t));
-	size_t * Qt = malloc(2*sizeof(size_t));
+	size_t * P = (size_t*) malloc(2*sizeof(size_t));
+	size_t * Qt = (size_t*) malloc(2*sizeof(size_t));
 	size_t r = RowEchelonForm_modular_double(101,2,2,A,2,P,Qt,false,FfpackSlabRecursive,true);
 	freducein_2_modular_double(101,2,2,A,2,false);
 	freducein_1_modular_double(101,4,A,1,false);
