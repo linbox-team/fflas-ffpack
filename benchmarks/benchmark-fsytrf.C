0c0e6a2e8e3c8e034f25c5905a06b33da7b39dc9
Clement Pernet <clement.pernet@imag.fr>
2016-11-04 09:39:20 +0100

    add a benchmark for fsytrf

---
+++ benchmarks/benchmark-fsytrf.C
@@ -0,0 +1,97 @@
+/* -*- mode: C++; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*- */
+// vim:sts=4:sw=4:ts=4:noet:sr:cino=>s,f0,{0,g0,(0,\:0,t0,+0,=s
+
+
+/* Copyright (c) FFLAS-FFPACK
+* Written by Clément Pernet <clement.pernet@imag.fr>
+* ========LICENCE========
+* This file is part of the library FFLAS-FFPACK.
+*
+* FFLAS-FFPACK is free software: you can redistribute it and/or modify
+* it under the terms of the  GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+* ========LICENCE========
+*/
+
+#include "fflas-ffpack/fflas-ffpack-config.h"
+#include <iostream>
+#include <givaro/modular.h>
+
+#include "fflas-ffpack/fflas-ffpack.h"
+#include "fflas-ffpack/utils/timer.h"
+#include "fflas-ffpack/utils/Matio.h"
+#include "fflas-ffpack/utils/args-parser.h"
+
+
+using namespace std;
+
+int main(int argc, char** argv) {
+  
+	size_t iter = 3;
+	int    q    = 131071;
+	size_t    n    = 2000;
+	bool up =true;
+	std::string file = "";
+  
+	Argument as[] = {
+		{ 'q', "-q Q", "Set the field characteristic (-1 for random).",  TYPE_INT , &q },
+		{ 'n', "-n N", "Set the dimension of the matrix.",               TYPE_INT , &n },
+		{ 'u', "-u yes/no", "Computes a UTDU (true) or LDLT decomposition (false).",  TYPE_BOOL , &up },
+		{ 'i', "-i R", "Set number of repetitions.",                     TYPE_INT , &iter },
+		{ 'f', "-f FILE", "Set the input file (empty for random).",  TYPE_STR , &file },
+		END_OF_ARGUMENTS
+	};
+
+	FFLAS::parseArguments(argc,argv,as);
+	
+	typedef Givaro::ModularBalanced<double> Field;
+	typedef Field::Element Element;
+	
+	Field F(q);
+	Field::Element * A;
+	
+	FFLAS::Timer chrono;
+	double time=0.0;
+	
+	FFLAS::FFLAS_UPLO uplo = up?FFLAS::FflasUpper:FFLAS::FflasLower;
+	for (size_t i=0;i<=iter;++i){
+		if (!file.empty()){
+			A = read_field(F, file.c_str(),  &n, &n);
+		}
+		else {
+			A = FFLAS::fflas_new<Element>(n*n);
+			Field::RandIter G(F);
+			FFPACK::RandomSymmetricMatrix (F, n, true, A, n, G);
+		}
+		
+		chrono.clear();
+		if (i) chrono.start();
+		FFPACK::fsytrf (F, uplo, n, A, n);
+		if (i) chrono.stop();
+		
+		time+=chrono.usertime();
+		FFLAS::fflas_delete( A);
+	}
+  
+		// -----------
+		// Standard output for benchmark - Alexis Breust 2014/11/14
+#define CUBE(x) ((x)*(x)*(x))
+	std::cout << "Time: " << time / double(iter)
+			  << " Gflops: " << CUBE(double(n)/1000.)/3. / time * double(iter);
+	FFLAS::writeCommandString(std::cout, as) << std::endl;
+	return 0;
+}
+
+
+
+
