215c3746bc8992134bf46ae9e3defaa4a75a259b
Clement Pernet <clement.pernet@imag.fr>
2017-03-24 09:10:55 +0100

    add test-fdot

---
+++ tests/test-fdot.C
@@ -0,0 +1,155 @@
+/* -*- mode: C++; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*- */
+// vim:sts=4:sw=4:ts=4:noet:sr:cino=>s,f0,{0,g0,(0,\:0,t0,+0,=s
+
+/*
+ * Copyright (C) 2017 FFLAS-FFPACK
+ * Written by Clément Pernet
+ * This file is Free Software and part of FFLAS-FFPACK.
+ *
+ * ========LICENCE========
+ * This file is part of the library FFLAS-FFPACK.
+ *
+ * FFLAS-FFPACK is free software: you can redistribute it and/or modify
+ * it under the terms of the  GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * ========LICENCE========
+ *.
+ */
+
+#define ENABLE_ALL_CHECKINGS 1
+
+#include "fflas-ffpack/fflas-ffpack-config.h"
+
+#include <iomanip>
+#include <iostream>
+
+#include "fflas-ffpack/fflas/fflas.h"
+#include "fflas-ffpack/utils/args-parser.h"
+#include "test-utils.h"
+#include "fflas-ffpack/utils/Matio.h"
+#include <givaro/modular.h>
+
+
+using namespace std;
+using namespace FFLAS;
+using Givaro::Modular;
+using Givaro::ModularBalanced;
+
+template<typename Field>
+bool check_fdot (const Field &F, size_t n,
+				 typename Field::ConstElement_ptr a, size_t inca,
+				 typename Field::ConstElement_ptr b, size_t incb){
+
+	
+	string ss = " inca = " + to_string (inca) + string(" incb = ") + to_string (incb);  
+	
+	cout<<std::left<<"Checking FDOT";
+	cout.fill('.');
+	cout.width(35);
+	cout<<ss;
+	FFLAS::Timer t; t.clear();
+	double time=0.0;
+	t.clear(); t.start();
+
+	typename Field::Element d = fdot (F, n, a, inca, b, incb);
+
+	t.stop();
+	time+=t.usertime();
+
+	typename Field::Element dcheck;
+	F.init(dcheck,F.zero);
+	for (size_t i = 0; i<n; i++)
+		F.axpyin (dcheck, a[i*inca], b[i*incb]);
+	if (F.areEqual(d,dcheck)){
+		cout << "PASSED ("<<time<<")"<<endl;
+		return true;
+	}else{
+		cout << "FAILED ("<<time<<") d = "<<d<<" dcheck = "<<dcheck<<endl;
+		return false;
+	}
+}
+
+template <class Field>
+bool run_with_field (Givaro::Integer q, size_t b, size_t n, size_t iters, uint64_t seed){
+	bool ok = true ;
+	int nbit=(int)iters;
+
+	while (ok &&  nbit){
+		Field* F= FFPACK::chooseField<Field>(q,b);
+		typename Field::RandIter G(*F,0,seed);
+		if (F==nullptr)
+			return true;
+
+		cout<<"Checking with ";F->write(cout)<<endl;
+
+		size_t inca = 1;//% + rand() % n;
+		size_t incb = 1;// + rand() % n;
+		typename Field::Element_ptr a = fflas_new (*F, n, inca);
+		typename Field::Element_ptr b = fflas_new (*F, n, incb);
+
+		FFPACK::RandomMatrix (*F, n, inca, a, inca, G);
+		FFPACK::RandomMatrix (*F, n, incb, b, incb, G);
+
+		ok &= check_fdot(*F,n,a,1,b,1);
+		ok &= check_fdot(*F,n,a,inca,b,incb);
+		ok &= check_fdot(*F,n,a,1,b,incb);
+		ok &= check_fdot(*F,n,a,inca,b,1);
+
+		fflas_delete(a);
+		fflas_delete(b);
+		nbit--;
+		delete F;
+	}
+	return ok;
+}
+
+int main(int argc, char** argv)
+{
+	cerr<<setprecision(10);
+	Givaro::Integer q=-1;
+	size_t b=0;
+	int n=2578;
+	size_t iters=10;
+	bool loop=false;
+	uint64_t seed = time(NULL);
+	Argument as[] = {
+		{ 'q', "-q Q", "Set the field characteristic (-1 for random).",         TYPE_INTEGER , &q },
+		{ 'b', "-b B", "Set the bitsize of the field characteristic.",  TYPE_INT , &b },
+		{ 'n', "-n N", "Set the  dimension.", TYPE_INT , &n },
+		{ 'i', "-i R", "Set number of repetitions.",            TYPE_INT , &iters },
+		{ 'l', "-loop Y/N", "run the test in an infinite loop.", TYPE_BOOL , &loop },
+		{ 's', "-s seed", "Set seed for the random generator", TYPE_INT, &seed },
+                END_OF_ARGUMENTS
+        };
+
+	parseArguments(argc,argv,as);
+
+	srand(seed);
+	bool ok = true;
+	do{
+		ok &= run_with_field<Modular<double> >(q,b,n,iters,seed);
+		ok &= run_with_field<ModularBalanced<double> >(q,b,n,iters,seed);
+		ok &= run_with_field<Modular<float> >(q,b,n,iters,seed);
+		ok &= run_with_field<ModularBalanced<float> >(q,b,n,iters,seed);
+		ok &= run_with_field<Modular<int32_t> >(q,b,n,iters,seed);
+		ok &= run_with_field<ModularBalanced<int32_t> >(q,b,n,iters,seed);
+		ok &= run_with_field<Modular<int64_t> >(q,b,n,iters,seed);
+		ok &= run_with_field<ModularBalanced<int64_t> >(q,b,n,iters,seed);
+		// ok &= run_with_field<Modular<Givaro::Integer> >(q,5,n/4+1,iters,seed);
+		// ok &= run_with_field<Modular<Givaro::Integer> >(q,(b?b:512),n/4+1,iters,seed);
+	} while (loop && ok);
+
+	if (!ok) std::cerr<<"with seed = "<<seed<<std::endl;
+
+	return !ok ;
+}
