c840d781a53ff060075ffbc7c67013233036b591
ledentp <philippe.ledent.1@gmail.com>
2017-05-12 11:02:37 +0200

    Modified Makefile.am according to recent name changes. Corrected comments in fflas_101_lvl1.C .

--- tutorial/fflas_101_lvl1.C
+++ tutorial/fflas_101_lvl1.C
@@ -22,11 +22,19 @@
 
 
 
-/*    =====================================================
-      |    Finit Field Linar Algebra Subprograms Level 1  |
-      =====================================================
-The puspose of this file is to be able to handle the first level of fflas.
-Here 
+/*    ====================================
+      |    FFLAS Crash Course Level 1    |
+      ====================================
+
+The puspose of this file is to be able to handle the first level
+of the linear algebra over finite fields used in fflas.
+In this example the finite field will be the Z over 101 Z set.
+This set is created using the givaro library.
+There had to be a choice between having a clean file and a clean display.
+The clean display has been adopted therefore the reader is invited
+to pay attention to what lines are used for display and what lines
+actually do the work.
+
  */
 
 // For information on the inclusions check the file 101-fflas.C
@@ -44,10 +52,11 @@ int main(int argc, char** argv) {
   Float_Ring F(120);
   Float_Ring::Element alpha,beta;    // scalars
   Float_Ring::Element * X, * Y, * Z; // vectors
+  const size_t xsize = 2, ysize = 2 , zsize = 2, one = 1, ld = 1;
   
-  X = fflas_new(F,2,1);
-  Y = fflas_new(F,2,1);
-  Z = fflas_new(F,2,1);
+  X = fflas_new(F,xsize,one);
+  Y = fflas_new(F,ysize,one);
+  Z = fflas_new(F,zsize,one);
 
   // ===== manual initialisation =====
   
@@ -55,15 +64,15 @@ int main(int argc, char** argv) {
   F.init(  X[1], 183);               // X[1] = 63
 
   std::cout << "\nInitialisation" << std::endl;
-  write_field(F, std::cout << "X:=", X, 2, 1, 1,true) << std::endl;
-  write_field(F, std::cout << "Y:=", Y, 2, 1, 1,true) << std::endl;
+  write_field(F, std::cout << "X:=", X, xsize, one,one,true) << std::endl;
+  write_field(F, std::cout << "Y:=", Y, ysize, one, one,true) << std::endl;
   
 
   // ===== initialisation from a vector ===== ???
   /*  finit(F,2,Y,1,X,1);
 
   write_field(F, std::cout << "X:=", X, 2, 1, 1,true) << std::endl;
-  write_field(F, std::cout << "Y:=", Y, 2, 1, 1,true) << std::endl;
+  write_field(F, std::cout << "Y:=", Y, ysize, one, one,true) << std::endl;
   */
 
   
@@ -79,9 +88,9 @@ int main(int argc, char** argv) {
 
   
   std::cout << "\nY mod 2 inplace" << std::endl;
-  write_field(F, std::cout << "Y:=", Y, 2, 1, 1,true) << std::endl;
+  write_field(F, std::cout << "Y:=", Y, ysize, one, ld,true) << std::endl;
   std::cout << "\nY := X mod 2" << std::endl;
-  write_field(F, std::cout << "Y:=", Y, 2, 1, 1,true) << std::endl;
+  write_field(F, std::cout << "Y:=", Y, ysize, one, ld,true) << std::endl;
 
 
   // ===== negation =====
@@ -90,9 +99,9 @@ int main(int argc, char** argv) {
 
   std::cout << "\n=== Negations ===" << std::endl;
   std::cout << "\nX := -X" << std::endl;
-  write_field(F, std::cout << "X:=", X, 2, 1, 1,true) << std::endl;
+  write_field(F, std::cout << "X:=", X, xsize, one, ld,true) << std::endl;
   std::cout << "\nY := -X" << std::endl;
-  write_field(F, std::cout << "Y:=", Y, 2, 1, 1,true) << std::endl;
+  write_field(F, std::cout << "Y:=", Y, ysize, one, ld,true) << std::endl;
   
 
   // ===== addition / substraction =====
@@ -101,17 +110,17 @@ int main(int argc, char** argv) {
   F.init(Y[0],2); F.assign(Y[1],F.one); // Y := (2;1)
 
   std::cout << "\n=== Additions / Substractions ===" << std::endl;
-  write_field(F, std::cout << "X:=", X, 2, 1, 1,true) << std::endl;
-  write_field(F, std::cout << "Y:=", Y, 2, 1, 1,true) << std::endl;
-  write_field(F, std::cout << "Z:=", Z, 2, 1, 1,true) << std::endl;
+  write_field(F, std::cout << "X:=", X, xsize, one, ld,true) << std::endl;
+  write_field(F, std::cout << "Y:=", Y, ysize, one, ld,true) << std::endl;
+  write_field(F, std::cout << "Z:=", Z, zsize, one, ld,true) << std::endl;
 
   fadd(F,2,X,1,Y,1,Z,1);  // Z := X + Y
   fsub(F,2,Z,1,Y,1,X,1);  // X := Z - Y
 
   std::cout << "\nZ := X + Y"  << std::endl;
-  write_field(F, std::cout << "Z:=", Z, 2, 1, 1,true) << std::endl;
+  write_field(F, std::cout << "Z:=", Z, zsize, one, ld,true) << std::endl;
   std::cout << "\nX := Z - Y"  << std::endl;
-  write_field(F, std::cout << "X:=", X, 2, 1, 1,true) << std::endl;
+  write_field(F, std::cout << "X:=", X, xsize, one, ld,true) << std::endl;
 
   // ===== scalar times vector =====
   F.init(alpha,42); // the scalar is scalar with respect to F
@@ -119,7 +128,7 @@ int main(int argc, char** argv) {
 
   std::cout << "\n=== Scalar times vector ===" << std::endl;
   write_field(F, std::cout << "X:=", X, 2, 1, 1,true) << std::endl;
-  write_field(F, std::cout << "Y:=", Y, 2, 1, 1,true) << std::endl;
+  write_field(F, std::cout << "Y:=", Y, ysize, one, one,true) << std::endl;
 
   
   
@@ -129,12 +138,12 @@ int main(int argc, char** argv) {
 
   std::cout << "\n=== Linear Combinations ==="  << std::endl;
   write_field(F, std::cout << "X:=", X, 2, 1, 1,true) << std::endl;
-  write_field(F, std::cout << "Y:=", Y, 2, 1, 1,true) << std::endl;
+  write_field(F, std::cout << "Y:=", Y, ysize, one, one,true) << std::endl;
 
   faxpy(F,2,alpha,X,1,Y,1); // Y := alpha * X + Y
 
   std::cout << "\nY := alpha * X + Y"  << std::endl;
-  write_field(F, std::cout << "Y:=", Y, 2, 1, 1,true) << std::endl;
+  write_field(F, std::cout << "Y:=", Y, ysize, one, one,true) << std::endl;
 
   // faxpby is a more general form of faxpy
 
@@ -144,21 +153,21 @@ int main(int argc, char** argv) {
   // ===== dot product (scalar product) =====
   
   std::cout << "\n=== Dot Product ==="  << std::endl;
-  write_field(F, std::cout << "Z:=", Z, 2, 1, 1,true) << std::endl;
-  write_field(F, std::cout << "X:=", X, 2, 1, 1,true) << std::endl;
+  write_field(F, std::cout << "Z:=", Z, zsize, one, ld,true) << std::endl;
+  write_field(F, std::cout << "X:=", X, xsize, one, ld,true) << std::endl;
   
   fdot(F,2,Z,1,X,1); // X := Z^T dot X
 
   std::cout << "\nX := Z^T dot X"  << std::endl;
-  write_field(F, std::cout << "X:=", X, 2, 1, 1,true) << std::endl;
+  write_field(F, std::cout << "X:=", X, xsize, one, ld,true) << std::endl;
 
   // ===== tests =====
   bool b;
   F.init(Z[0], 0); F.init(Z[1], 120);
   
   std::cout << "\n=== Tests ==="  << std::endl;
-  write_field(F, std::cout << "Z:=", Z, 2, 1, 1,true) << std::endl;
-  write_field(F, std::cout << "X:=", X, 2, 1, 1,true) << std::endl;
+  write_field(F, std::cout << "Z:=", Z, zsize, one, ld,true) << std::endl;
+  write_field(F, std::cout << "X:=", X, xsize, one, ld,true) << std::endl;
 
   
   b = fiszero(F,2,Z,1); // TRUE if X is zero vector in F
@@ -172,14 +181,14 @@ int main(int argc, char** argv) {
   // ===== swap  =====
 
   std::cout << "\n=== Swap ==="  << std::endl;
-  write_field(F, std::cout << "X:=", X, 2, 1, 1,true) << std::endl;
-  write_field(F, std::cout << "Y:=", Y, 2, 1, 1,true) << std::endl;
+  write_field(F, std::cout << "X:=", X, xsize, one, ld,true) << std::endl;
+  write_field(F, std::cout << "Y:=", Y, ysize, one, ld,true) << std::endl;
 
   fswap(F,2,X,1,Y,1);   // exchanges X and Y;
 
   std::cout << "\nX <-> Y"  << std::endl;
-  write_field(F, std::cout << "X:=", X, 2, 1, 1,true) << std::endl;
-  write_field(F, std::cout << "Y:=", Y, 2, 1, 1,true) << std::endl;
+  write_field(F, std::cout << "X:=", X, xsize, one, ld,true) << std::endl;
+  write_field(F, std::cout << "Y:=", Y, ysize, one, ld,true) << std::endl;
   
   
 } // End main
