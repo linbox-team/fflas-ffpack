1721458032e7196b016ef187817e10ccc19922fe
ledentp <philippe.ledent.1@gmail.com>
2017-05-31 15:25:05 +0200

    modified test-ftrtri to comply to new standards

--- tests/test-ftrtri.C
+++ tests/test-ftrtri.C
@@ -1,5 +1,5 @@
-/* -*- mode: C++; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
-// vim:sts=8:sw=8:ts=8:noet:sr:cino=>s,f0,{0,g0,(0,\:0,t0,+0,=s
+/* -*- mode: C++; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*- */
+// vim:sts=4:sw=4:ts=4:noet:sr:cino=>s,f0,{0,g0,(0,\:0,t0,+0,=s
 
 /*
  * Copyright (C) FFLAS-FFPACK
@@ -26,105 +26,139 @@
  *.
  */
 
-//--------------------------------------------------------------------------
-//                        Test for ftrtri : 1 computation
-//
-//--------------------------------------------------------------------------
-// Clement Pernet
-//-------------------------------------------------------------------------
+#define  __FFLASFFPACK_SEQUENTIAL
 
-#define TIME 1
-
-#include <iomanip>
-#include <iostream>
-#include "givaro/modular-balanced.h"
+#define ENABLE_ALL_CHECKINGS 1
 
 #include "fflas-ffpack/fflas-ffpack-config.h"
 
+#include <iomanip>
+#include <iostream>
+
 #include "fflas-ffpack/utils/timer.h"
-#include "fflas-ffpack/utils/Matio.h"
-#include "fflas-ffpack/ffpack/ffpack.h"
-
-
+#include "fflas-ffpack/fflas/fflas.h"
+#include "fflas-ffpack/utils/args-parser.h"
+#include "test-utils.h"
+#include <givaro/modular.h>
 
 using namespace std;
 using namespace FFPACK;
+using namespace FFLAS;
+using Givaro::Modular;
+using Givaro::ModularBalanced;
 
-typedef Givaro::ModularBalanced<double> Field;
+
+template<typename Field, class RandIter>
+bool check_ftrtri (const Field &F, size_t n, FFLAS_UPLO uplo, FFLAS_TRANSPOSE trans, FFLAS_DIAG diag, RandIter& Rand){
+    typedef typename Field::Element Element;
+    Element * A, * B;
+    size_t lda = n + (rand() % n );
+    A  = fflas_new(F,n,lda);
+    B  = fflas_new(F,n,lda);
+    
+    RandomTriangularMatrix (F, n, n, uplo, diag, true, A, lda, Rand);
+    fassign (F, n, n, A, lda, B, lda);
+    
+    string ss=string((uplo == FflasLower)?"Lower_":"Upper_")+string((trans == FflasTrans)?"Trans_":"NoTrans_")+string((diag == FflasUnit)?"Unit":"NonUnit");
+
+    cout<<std::left<<"Checking FTRTRI_";
+    cout.fill('.');
+    cout.width(30);
+    cout<<ss;
+
+
+    Timer t; t.clear();
+    double time=0.0;
+    t.clear();
+    t.start();
+    ftrtri (F, uplo, diag, n, A, lda);
+    t.stop();
+    time+=t.usertime();
+
+    // B <- A times B
+    ftrmm(F, FFLAS::FflasLeft, uplo, trans, diag, n, n, F.one, A, lda, B, lda);
+
+    // Is B the identity matrix ?
+    bool ok = true;
+    for(size_t li = 0; li < n && ok; li++){
+	    for(size_t co = 0; co < n && ok; co++){
+		    ok = ((li == co) && (F.areEqual(A[li*(lda+1)],F.one))) || (F.areEqual(A[li*lda+co],F.zero));
+	    }
+    }
+	
+    if (ok){
+	    cout << "PASSED ("<<time<<")"<<endl;
+    } else{
+	    cout << "FAILED ("<<time<<")"<<endl;
+    }
+
+    fflas_delete(A);
+    fflas_delete(B);
+    return ok;
+}
+template <class Field>
+bool run_with_field (Givaro::Integer q, size_t b, size_t n, size_t iters, uint64_t seed){
+    bool ok = true ;
+    int nbit=(int)iters;
+    
+    while (ok &&  nbit){
+	    //typedef typename Field::Element Element ;
+	    // choose Field
+	    Field* F= chooseField<Field>(q,b);
+	    typename Field::RandIter G(*F,0,seed);
+	    if (F==nullptr)
+		    return true;
+	    
+	    cout<<"Checking with ";F->write(cout)<<endl;
+	    
+	    ok = ok && check_ftrtri(*F,n,FflasLower,FflasNoTrans,FflasUnit,G);
+	    ok = ok && check_ftrtri(*F,n,FflasUpper,FflasNoTrans,FflasUnit,G);
+	    ok = ok && check_ftrtri(*F,n,FflasLower,FflasTrans,FflasUnit,G);
+	    ok = ok && check_ftrtri(*F,n,FflasUpper,FflasTrans,FflasUnit,G);
+	    ok = ok && check_ftrtri(*F,n,FflasLower,FflasNoTrans,FflasNonUnit,G);
+	    ok = ok && check_ftrtri(*F,n,FflasUpper,FflasNoTrans,FflasNonUnit,G);
+	    ok = ok && check_ftrtri(*F,n,FflasLower,FflasTrans,FflasNonUnit,G);
+	    ok = ok && check_ftrtri(*F,n,FflasUpper,FflasTrans,FflasNonUnit,G);
+	    nbit--;
+	    delete F;
+    }
+    return ok;
+}
 
 int main(int argc, char** argv)
 {
-
-	size_t  n;
-	int nbit=atoi(argv[3]); // number of times the product is performed
-	cerr<<setprecision(10);
-
-	if (argc != 4)	{
-		cerr<<"Usage : test-ftrtri <p> <A> <<i>"
-		    <<endl
-		    <<"         to invert a triangular matrix  A mod p (i computations)"
-		    <<endl;
-		exit(-1);
-	}
-	Field F(atoi(argv[1]));
-	Field::Element * A,*Ab;
-	A = read_field(F,argv[2],&n,&n);
-	Ab = FFLAS::fflas_new<Field::Element>(n*n);
-
-	for (int i=0; i<n;++i){
-		for(int j=i+1; j<n; ++j)
-			F.assign(*(Ab+i*n+j),*(A+i*n+j));
-		F.assign(*(Ab+i*(n+1)), 1.0);
-		for(int j=0; j<i; ++j)
-			F.assign(*(Ab+i*n+j),0.0);
-	}
-
-
-	Field::Element * X = FFLAS::fflas_new<Field::Element>(n*n);
-
- FFLAS::Timer tim,t; t.clear();tim.clear();
-
-	for(int i = 0;i<nbit;++i){
-		t.clear();
-		t.start();
-//				FFPACK::trinv_left (F, n, A, n, X, n);
-				FFPACK::ftrtri(F, FFLAS::FflasUpper, FFLAS::FflasUnit, n, A, n);
-		t.stop();
-		tim+=t;
-		if (i+1<nbit)
-			for (int i=0; i<n*n;++i)
-				F.assign(*(A+i),*(Ab+i));
-	}
-
-#ifdef __FFLASFFPACK_DEBUG
-	FFLAS::ftrmm (F, FFLAS::FflasRight, FFLAS::FflasUpper, FFLAS::FflasNoTrans, FFLAS::FflasUnit,
-		      n, n, 1.0,
-		      A,n, Ab, n);
-	bool wrong = false;
-
-	for (int i=0;i<n;++i)
-		for (int j=0;j<n;++j)
-			if ( ((i!=j) && !F.isZero(*(Ab+i*n+j)))
-			     ||((i==j) &&!F.isOne(*(Ab+i*n+j))))
-				wrong = true;
-
-	if ( wrong ){
-		cerr<<"FAIL"<<endl;
-		write_field (F,cerr<<"Ab="<<endl,Ab,n,n,n);
-		    //write_field (F,cerr<<"X="<<endl,X,n,n,n);
-	}else{
-
-		cerr<<"PASS"<<endl;
-	}
-#endif
-
-#if TIME
-	double gflops = 1.0/3.0*(n/1000.0*n/1000000.0)*nbit*n/tim.usertime();
-	cerr<<"n = "<<n<<" Inversion over Z/"<<atoi(argv[1])<<"Z : t= "
-	     << tim.usertime()/nbit
-	     << " s, Gfops = "<<gflops
-	     << endl;
-
-	cout<<n<<" "<<gflops<<" "<<tim.usertime()/nbit<<endl;
-#endif
+    cerr<<setprecision(10);
+    Givaro::Integer q=-1;
+    size_t b=0;
+    size_t n=483;
+    size_t iters=1;
+    bool loop=false;
+    uint64_t seed = time(NULL);
+    Argument as[] = {
+	    { 'q', "-q Q", "Set the field characteristic (-1 for random).",         TYPE_INTEGER , &q },
+	    { 'b', "-b B", "Set the bitsize of the field characteristic.",  TYPE_INT , &b },
+	    { 'n', "-n N", "Set the dimension of the system.", TYPE_INT , &n },
+	    { 'i', "-i R", "Set number of repetitions.",            TYPE_INT , &iters },
+	    { 'l', "-loop Y/N", "run the test in an infinite loop.", TYPE_BOOL , &loop },
+	    { 's', "-s seed", "Set seed for the random generator", TYPE_INT, &seed },
+	    END_OF_ARGUMENTS
+    };
+    
+    parseArguments(argc,argv,as);
+    
+    bool ok = true;
+    do{
+	    ok &= run_with_field<Modular<double> >(q,b,n,iters,seed);
+	    ok &= run_with_field<ModularBalanced<double> >(q,b,n,iters,seed);
+	    ok &= run_with_field<Modular<float> >(q,b,n,iters,seed);
+	    ok &= run_with_field<ModularBalanced<float> >(q,b,n,iters,seed);
+	    ok &= run_with_field<Modular<int32_t> >(q,b,n,iters,seed);
+	    ok &= run_with_field<ModularBalanced<int32_t> >(q,b,n,iters,seed);
+	    ok &= run_with_field<Modular<int64_t> >(q,b,n,iters,seed);
+	    ok &= run_with_field<ModularBalanced<int64_t> >(q,b,n,iters,seed);
+	    ok &= run_with_field<Modular<Givaro::Integer> >(q,5,n/4+1,iters,seed);
+	    ok &= run_with_field<Modular<Givaro::Integer> >(q,(b?b:512),n/4+1,iters,seed);
+    } while (loop && ok);
+    
+    return !ok ;
 }
