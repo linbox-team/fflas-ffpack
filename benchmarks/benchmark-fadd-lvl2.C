5e2ca205a5d1f800eafadc97667b24194b87eab8
ledentp <philippe.ledent.1@gmail.com>
2017-06-09 15:06:58 +0200

    benchmark-fadd-lvl2.C : made fixes requested in review

--- benchmarks/benchmark-fadd-lvl2.C
+++ benchmarks/benchmark-fadd-lvl2.C
@@ -3,7 +3,7 @@
 
 
 /* Copyright (c) FFLAS-FFPACK
- * Written by Clément Pernet <clement.pernet@imag.fr>
+ * Written by Philippe LEDENT <philippe.ledent@etu.univ-grenoble-alpes.fr>
  * ========LICENCE========
  * This file is part of the library FFLAS-FFPACK.
  *
@@ -37,64 +37,64 @@ using namespace std;
 using namespace FFLAS;
 using namespace FFPACK;
 int main(int argc, char** argv) {
-  
-  size_t iter      = 3;
-  int    q         = 131071;
-  size_t rows      = 1000;
-  size_t cols      = 1000;
-  double a         = 1.0;
-  size_t threshold = 64;
-  std::string file = "";
-  
-  Argument as[] = {
-    { 'q', "-q Q", "Set the field characteristic (-1 for random).",         TYPE_INT , &q },
-	{ 'm', "-m M", "Set the row dimension of the matrix C.",                TYPE_INT , &rows },
-	{ 'n', "-n N", "Set the column dimension of the matrix C.",             TYPE_INT , &cols },
-    { 'a', "-a A", "Set the value of the coefficient alpha for alpha * B.", TYPE_DOUBLE , &a},
-    { 'i', "-i R", "Set number of repetitions.",                            TYPE_INT , &iter },
-    END_OF_ARGUMENTS
-  };
 
-  FFLAS::parseArguments(argc,argv,as);
-	
-  typedef Givaro::ModularBalanced<double> Field;
-  typedef Field::Element Element;
-	
-  Field F(q);
-  Field::Element_ptr A, B;
-  Field::Element_ptr C;
-  Element alpha;
-  F.init(alpha, a);
-
-	
-  FFLAS::Timer chrono;
-  double time=0.0;
-	
-  for (size_t i=0;i<=iter;++i){
-	A = fflas_new(F, rows, cols);
-    size_t lda = cols;
-    B = fflas_new(F, rows, cols);
-    size_t ldb = cols;
-    C = fflas_new(F, rows, cols);
-    size_t ldc = cols;
-    Field::RandIter G(F);
-    RandomMatrix (F, rows, cols, A, lda, G);
-    RandomMatrix (F, rows, cols, B, ldb, G);
-    chrono.clear();
-    if (i) chrono.start();
-    fadd(F, rows, cols, A, lda, alpha, B, ldb, C, ldc);
-    if (i) chrono.stop();
-		
-    time+=chrono.usertime();
-    FFLAS::fflas_delete(A);
-    FFLAS::fflas_delete(B);
-    FFLAS::fflas_delete(C);
-  }
+	size_t iter      = 3;
+	int    q         = 131071;
+	size_t rows      = 5000;
+	size_t cols      = 5000;
+	double a         = 1.0;
+	size_t threshold = 64;
+	std::string file = "";
   
-  // -----------
-  // Standard output for benchmark - Alexis Breust 2014/11/14
-  std::cout << "Time: " << time / double(iter)
-			<< " Gfops: " << ((double(rows)/1000)*(double(cols)/1000))/(1000*time)* double(iter); //(n^2/1000^3)/time * iter
-  FFLAS::writeCommandString(std::cout, as) << std::endl;
-  return 0;
+	Argument as[] = {
+		{ 'q', "-q Q", "Set the field characteristic (-1 for random).",         TYPE_INT , &q },
+		{ 'm', "-m M", "Set the row dimension of the matrix C.",                TYPE_INT , &rows },
+		{ 'n', "-n N", "Set the column dimension of the matrix C.",             TYPE_INT , &cols },
+		{ 'a', "-a A", "Set the value of the coefficient alpha for alpha * B.", TYPE_DOUBLE , &a},
+		{ 'i', "-i R", "Set number of repetitions.",                            TYPE_INT , &iter },
+		END_OF_ARGUMENTS
+	};
+  
+	FFLAS::parseArguments(argc,argv,as);
+  
+	typedef Givaro::ModularBalanced<double> Field;
+	typedef Field::Element Element;
+	
+	Field F(q);
+	Field::Element_ptr A, B;
+	Field::Element_ptr C;
+	Element alpha;
+	F.init(alpha, a);
+  
+  
+	FFLAS::Timer chrono;
+	double time=0.0;
+  
+	for (size_t i=0;i<=iter;++i){
+		A = fflas_new(F, rows, cols);
+		size_t lda = cols;
+		B = fflas_new(F, rows, cols);
+		size_t ldb = cols;
+		C = fflas_new(F, rows, cols);
+		size_t ldc = cols;
+		Field::RandIter G(F);
+		RandomMatrix (F, rows, cols, A, lda, G);
+		RandomMatrix (F, rows, cols, B, ldb, G);
+		chrono.clear();
+		if (i) chrono.start();
+		fadd(F, rows, cols, A, lda, alpha, B, ldb, C, ldc);
+		if (i) chrono.stop();
+	
+		time+=chrono.usertime();
+		FFLAS::fflas_delete(A);
+		FFLAS::fflas_delete(B);
+		FFLAS::fflas_delete(C);
+	}
+
+	// -----------
+	// Standard output for benchmark - Alexis Breust 2014/11/14
+	std::cout << "Time: " << time / double(iter)
+			  << " Gfops: " << ((double(rows)/1000)*(double(cols)/1000))/(1000*time)* double(iter); //(n^2/1000^3)/time * iter
+	FFLAS::writeCommandString(std::cout, as) << std::endl;
+	return 0;
 }
