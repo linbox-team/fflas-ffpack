308d187d8c537da77965c163e62c91eefe18db10
Jean-Guillaume Dumas <Jean-Guillaume.Dumas@imag.fr>
2017-06-21 15:57:38 +0200

    givprint in givaro now

--- fflas-ffpack/utils/print-utils.h
+++ fflas-ffpack/utils/print-utils.h
@@ -27,126 +27,13 @@
 
 #include <fflas-ffpack/fflas-ffpack-config.h>
 #include <math.h>
-#include <vector>
-// #include <pair>
-#include <list>
-#include <set>
-#include <iterator>
-#include "gmp++/gmp++.h"
+#include <givaro/givprint.h>
+#include <givaro/givinteger.h>
 
 namespace std
 {
 
-	/*! Prints a vector on output.
-	 * @param o output stream
-	 * @param v vector
-	 * @warning <<(ostream&,T&) exists !
-	 */
-	template<class T, class Alloc>
-	std::ostream & operator<<(std::ostream&o, const std::vector<T, Alloc> & v)
-	{
-		o << '[' ;
-		std::copy(v.begin(), v.end(), std::ostream_iterator<T>(o, " "));
-		// if (v.size()) {
-		// 	size_t i = 0  ;
-		// 	for (; i < v.size()-1 ; ++i)
-		// 		o << v[i] << ',' ;
-		// 	o <<  v[i] ;
-		// }
-		return o << ']' ;
-	}
 
-
-	/*! Prints a pair.
-	 * @param o output stream
-	 * @param C a pair
-	 * @warning <<(ostream&,T&) exists !
-	 */
-	template<class S, class T>
-	std::ostream& operator<<(std::ostream& o, const std::pair<S, T> & C)
-	{
-		o << '(' << C.first << ", " << C.second << ')';
-		return o ;
-	}
-
-
-	/*! Prints a list.
-	 * @param o output stream
-	 * @param C a pair
-	 * @warning <<(ostream&,T&) exists !
-	 */
-	template<class T, class Alloc>
-	std::ostream& operator<< (std::ostream& o, const std::list<T, Alloc> & L)
-	{
-		o << '{' ;
-		std::copy(L.begin(), L.end(), std::ostream_iterator<T>(o, " "));
-		return o << '}' ;
-
-		// typename std::list<T>::const_iterator it = L.begin() ;
-		// if (it != L.end() )
-		// 	while(true) {
-		// 		o << *it ;
-		// 		if (++it != L.end())
-		// 			o << ", " ;
-		// 		else
-		// 			break;
-		// 	}
-	}
-
-
-	/*! Prints a set.
-	 * @param o output stream
-	 * @param C a pair
-	 * @warning <<(ostream&,T&) exists !
-	 */
-	template<class T, class Alloc>
-	std::ostream& operator<< (std::ostream& o, const std::set<T, Alloc> & S)
-	{
-		o << '|' ;
-		std::copy(S.begin(), S.end(), std::ostream_iterator<T>(o, " "));
-		return o << '|' ;
-		// typename std::set<T>::const_iterator it = L.begin() ;
-		// o << '|' ;
-		// if (it != L.end() )
-		// 	while(true) {
-		// 		o << *it ;
-		// 		if (++it != L.end())
-		// 			o << ", " ;
-		// 		else
-		// 			break;
-		// 	}
-		// return o << '|' ;
-	}
-
-
-#if 0
-	std::ostream &operator << (std::ostream &out, const std::vector<bool> &S)
-	{
-		std::vector<bool>::const_iterator i;
-
-		for (i = S.begin (); i != S.end (); ++i) {
-			out << ((*i) ? "1" : "0");
-			if (i != S.end () - 1)
-				out << ", ";
-		}
-
-		return out;
-	}
-
-	template<class T, template <class T> class Container>
-	std::ostream& operator<< (std::ostream& o, const Container<T>& C)
-	{
-		for(typename Container<T>::const_iterator refs =  C.begin();
-		    refs != C.end() ;
-		    ++refs )
-			o << (*refs) << " " ;
-		return o << std::endl;
-	}
-
-
-#endif
-
-	
 	template<typename T>
 	std::ostream& write_matrix(std::ostream& out, Givaro::Integer p, size_t m, size_t n, T* C, size_t ldc){
 		
