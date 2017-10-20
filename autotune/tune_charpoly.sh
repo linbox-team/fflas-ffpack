d8509928bc78d45acdc77ca8d208f9524986a74f
Clement Pernet <clement.pernet@imag.fr>
2017-10-20 14:44:01 +0200

    fix autotune when compiling in a distinct directory

--- autotune/tune_charpoly.sh
+++ autotune/tune_charpoly.sh
@@ -5,4 +5,4 @@ echo =================================================
 echo 
 ./charpoly-LUK-ArithProg 2> charpoly-LUK-ArithProg-threshold.h  | tee charpoly-LUK-ArithProg-autotune.log
 ./charpoly-Danilevskii-LUK 2> charpoly-Danilevskii-LUK-threshold.h  | tee charpoly-Danilevskii-LUK-autotune.log
-./arithprog 8 160 4 4 1000 3 2> arithprog-blocksize.h  | tee arithprog-blocksize-autotune.log
+./arithprog 8 160 4 4 600 3 2> arithprog-blocksize.h  | tee arithprog-blocksize-autotune.log
