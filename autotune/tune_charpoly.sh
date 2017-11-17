9ac281be8c78973cde6f77c06783e0d09b06ccbb
Clement Pernet <clement.pernet@imag.fr>
2017-11-17 15:53:47 +0100

    reverse order in autotuning

--- autotune/tune_charpoly.sh
+++ autotune/tune_charpoly.sh
@@ -3,8 +3,8 @@ echo =================================================
 echo ========= FFLAS-FFPACK CharPoly Autotuning =========
 echo =================================================
 echo 
+(./arithprog 8 160 4 4 600 3 > arithprog-blocksize.h) 2>&1 | tee arithprog-blocksize-autotune.log
+
 (./charpoly-LUK-ArithProg > charpoly-LUK-ArithProg-threshold.h) 2>&1 | tee charpoly-LUK-ArithProg-autotune.log
 
 (./charpoly-Danilevskii-LUK > charpoly-Danilevskii-LUK-threshold.h) 2>&1 | tee charpoly-Danilevskii-LUK-autotune.log
-
-(./arithprog 8 160 4 4 600 3 > arithprog-blocksize.h) 2>&1 | tee arithprog-blocksize-autotune.log
