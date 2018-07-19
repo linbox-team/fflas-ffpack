1c5b037afa5c9fdb46621d3fb2732a6af5cabaa7
Clement Pernet <clement.pernet@imag.fr>
2018-07-19 11:40:30 -0400

    clean up

--- benchmarks/benchmark-fgemm.C
+++ benchmarks/benchmark-fgemm.C
@@ -82,11 +82,11 @@ int main(int argc, char** argv) {
 
 	if (NBK==-1) NBK = t;
 //  typedef Givaro::Modular<Givaro::Integer> Field;
-  typedef Givaro::Modular<int64_t> Field;
-//    typedef Givaro::Modular<double> Field;
+//  typedef Givaro::Modular<int64_t> Field;
+//  typedef Givaro::Modular<double> Field;
 //	typedef Givaro::Modular<float> Field;
 //	typedef Givaro::ModularBalanced<float> Field;
-//	typedef Givaro::ModularBalanced<double> Field;
+	typedef Givaro::ModularBalanced<double> Field;
 //	typedef Givaro::Modular<Givaro::Integer> Field;
 	typedef Field::Element Element;
 
