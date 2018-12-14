0e6fb28826af722107adb8be9fb8879e797726a1
Clément Pernet <clement.pernet@gmail.com>
2018-12-14 16:56:21 +0100

    final lifting for benchmark-fgesv

--- benchmarks/benchmark-fgesv.C
+++ benchmarks/benchmark-fgesv.C
@@ -38,12 +38,10 @@ int main(int argc, char** argv) {
 	size_t iter = 3;
 	int    q    = 131071;
 	size_t    m    = 2000 ;
-	size_t    n    = 2000;
+	size_t    n    = 1000;
 	std::string file1 = "";
 	std::string file2 = "";
-	int t=MAX_THREADS;
-	int NBK = -1;
-    //	int p = 0; // 0 for sequential 1 for pIter-sRec ; 2 for pRec; 3 for hybrid
+    bool v=false;
 
 	Argument as[] = {
 		{ 'q', "-q Q", "Set the field characteristic (-1 for random).",  TYPE_INT , &q },
@@ -52,21 +50,16 @@ int main(int argc, char** argv) {
 		{ 'i', "-i R", "Set number of repetitions.",               TYPE_INT , &iter },
 		{ 'f', "-f FILE", "Set the first input file (empty for random).", TYPE_STR, &file1 },
 		{ 'g', "-g FILE", "Set the second input file (empty for random).", TYPE_STR, &file2 },
-		{ 't', "-t T", "number of virtual threads to drive the partition.", TYPE_INT, &t },
-		{ 'b', "-b B", "number of numa blocks per dimension for the numa placement", TYPE_INT, &NBK },
-        //		{ 'p', "-p P", "0 for sequential, 1 for Iterative, 2 for Recursive, 3 for Hybrid.", TYPE_INT , &p },
+        { 'v', "-v V", "Whether to run the verification of the solution.", TYPE_BOOL, &v },
 		END_OF_ARGUMENTS
 	};
 
 	FFLAS::parseArguments(argc,argv,as);
-	if (NBK==-1) NBK = t;
 	
 	typedef Givaro::ModularBalanced<double> Field;
-	typedef Field::Element Element;
 
 	Field F(q);
-	Element * A;
-	Element * B;
+    Field::Element_ptr  A, Ac, B, Bc;
 
 	FFLAS::Timer chrono;
 	double time=0.0;
@@ -77,70 +70,51 @@ int main(int argc, char** argv) {
 	}
 	else{
 		A = FFLAS::fflas_new (F,m,m,Alignment::CACHE_PAGESIZE);
-		PAR_BLOCK{ FFLAS::pfrand(F,G,m,m,A,m/NBK); }
-	
-		for (size_t k=0;k<(size_t)m;++k)
-			while (F.isZero( G.random(*(A+k*(m+1)))));
-	}
+		FFPACK::RandomMatrixWithRank (F, m,m, m, A, m);
+    }
 
 	if (!file2.empty()){
 	    FFLAS::ReadMatrix (file2.c_str(),F,m,n,B);
 	}
 	else{
 		B = FFLAS::fflas_new(F,m,n,Alignment::CACHE_PAGESIZE);
-		PAR_BLOCK{ FFLAS::pfrand(F,G,m,n,B,m/NBK); }
+		FFPACK::RandomMatrix(F, m, n, B, n, G);
 	}
-		//}
-  
-	for (size_t i=0;i<=iter;++i){
-		chrono.clear();
-		if (i) chrono.start();
 
-        //		if (!p){
-        int info;
-        FFPACK::fgesv (F, FFLAS::FflasLeft, m,n, A, m, B, n, &info);
-		// }
-		// else{
-		// 	FFLAS::ParSeqHelper::Parallel<FFLAS::CuttingStrategy::Block,FFLAS::StrategyParameter::Threads> PSH(t);
-		// 	PAR_BLOCK{
-		// 		switch (p) {
-		// 			case 1: {
-		// 				FFLAS::TRSMHelper<FFLAS::StructureHelper::Iterative,
-		// 								  FFLAS::ParSeqHelper::Parallel<FFLAS::CuttingStrategy::Block,FFLAS::StrategyParameter::Threads> > 
-		// 								  PH (PSH);
-		// 				FFLAS::ftrsm (F, FFLAS::FflasLeft, FFLAS::FflasLower, 
-		// 							  FFLAS::FflasNoTrans, FFLAS::FflasNonUnit, 
-		// 							  m,n, F.one, A, m, B, n, PH);
-		// 				break;}
-		// 			case 2: {FFLAS::TRSMHelper<FFLAS::StructureHelper::Recursive, 
-		// 									   FFLAS::ParSeqHelper::Parallel<FFLAS::CuttingStrategy::Block,FFLAS::StrategyParameter::Threads> > 
-		// 					PH (PSH);
-		// 				FFLAS::ftrsm (F, FFLAS::FflasLeft, FFLAS::FflasLower, 
-		// 							  FFLAS::FflasNoTrans, FFLAS::FflasNonUnit, 
-		// 							  m,n, F.one, A, m, B, n, PH); 
-		// 				break;}
-		// 			case 3: 
-		// 				FFLAS::TRSMHelper<FFLAS::StructureHelper::Hybrid, FFLAS::ParSeqHelper::Parallel<FFLAS::CuttingStrategy::Block,FFLAS::StrategyParameter::Threads> > 
-		// 					PH (PSH);
-		// 				FFLAS::ftrsm (F, FFLAS::FflasLeft, FFLAS::FflasLower, 
-		// 							  FFLAS::FflasNoTrans, FFLAS::FflasNonUnit, 
-		// 							  m,n, F.one, A, m, B, n, PH);
-		// 				break;
-		// 		}
-				
-		// 	}
-		// }
+    Bc = FFLAS::fflas_new(F,m,n);
+    Ac = FFLAS::fflas_new (F,m,m,Alignment::CACHE_PAGESIZE);
+    
+	for (size_t i=0;i<=iter;++i){
+        FFLAS::fassign (F,m,m,A,m,Ac,m);
+        FFLAS::fassign (F,m,n,B,n,Bc,n);
+		int info;
+        chrono.clear();
+		if (i) chrono.start();
+        FFPACK::fgesv (F, FFLAS::FflasLeft, m,n, Ac, m, Bc, n, &info);
 		if (i) {chrono.stop(); time+=chrono.realtime();}
 	}
 	
-	FFLAS::fflas_delete( A);
-	FFLAS::fflas_delete( B);
-	
-		// -----------
+        // -----------
 		// Standard output for benchmark - Alexis Breust 2014/11/14
 	std::cout << "Time: " << time / double(iter)
 			  << " Gfops: " << (double(m*m)/1000000.*(2*double(m)/3000.+2*double(n)/1000.0)) / time * double(iter);
 	FFLAS::writeCommandString(std::cout, as) << std::endl;
+
+    if (v){
+        FFLAS::fgemm(F,FFLAS::FflasNoTrans,FFLAS::FflasNoTrans,m,n,m,F.one,A,m,Bc,n,F.mOne,B,n);
+        if (!FFLAS::fiszero(F,m,n,B,n)){
+            std::cerr<<"Verification FAILED"<<std::endl;
+            // FFLAS::WriteMatrix(std::cerr<<"A= "<<std::endl,F,m,m,A,m);
+            // FFLAS::WriteMatrix(std::cerr<<"X= "<<std::endl,F,m,n,Bc,n);
+            // FFLAS::WriteMatrix(std::cerr<<"B= "<<std::endl,F,m,n,B,n);
+            FFLAS::fflas_delete( A);
+            FFLAS::fflas_delete( B);
+            return -1;
+        }
+    }
+    FFLAS::fflas_delete( A);
+	FFLAS::fflas_delete( B);
+	
 
 	return 0;
 }
