61305dba2665b93ae4275332918b4542aef3966c
Vincent Neiger <vincent.neiger@unilim.fr>
2019-02-08 15:37:06 +0100

    fix code style in autotune

--- autotune/winograd.C
+++ autotune/winograd.C
@@ -1,6 +1,3 @@
-/* -*- mode: C++; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*- */
-// vim:sts=4:sw=4:ts=4:noet:sr:cino=>s,f0,{0,g0,(0,\:0,t0,+0,=s
-
 /*
  * Copyright (C) 2012 FFLAS-FFPACK group.
  *
@@ -43,13 +40,13 @@
 template<class Field>
 bool balanced(const Field & )
 {
-	return false;
+    return false;
 }
 
 template <class T>
 bool balanced(const Givaro::ModularBalanced<T>&)
 {
-	return true;
+    return true;
 }
 
 #ifdef __GIVARO_USE_OPENMP
@@ -63,122 +60,124 @@ typedef Givaro::Timer TTimer;
 #include <ctime>
 
 int main () {
-	using namespace std;
+    using namespace std;
 
-	typedef FIELD Field;
-	Field F(17);
-	typedef Field::Element Element ;
-	size_t n=512, nmax=4000, prec=512, nbest=0, count=0;
-	TTimer chrono;
-	bool bound=false;
+    typedef FIELD Field;
+    Field F(17);
+    typedef Field::Element Element ;
+    size_t n=512, nmax=4000, prec=512, nbest=0, count=0;
+    TTimer chrono;
+    bool bound=false;
 
-	Element * A = FFLAS::fflas_new (F,nmax,nmax);
-	Element * B = FFLAS::fflas_new (F,nmax,nmax);
-	Element * C = FFLAS::fflas_new (F,nmax,nmax);
-	FFPACK::RandomMatrix (F, nmax, nmax, A, nmax);
-	FFPACK::RandomMatrix (F, nmax, nmax, B, nmax);
-	FFPACK::RandomMatrix (F, nmax, nmax, C, nmax);
+    Element * A = FFLAS::fflas_new (F,nmax,nmax);
+    Element * B = FFLAS::fflas_new (F,nmax,nmax);
+    Element * C = FFLAS::fflas_new (F,nmax,nmax);
+    FFPACK::RandomMatrix (F, nmax, nmax, A, nmax);
+    FFPACK::RandomMatrix (F, nmax, nmax, B, nmax);
+    FFPACK::RandomMatrix (F, nmax, nmax, C, nmax);
 
-	time_t result = std::time(NULL);
-	cerr << std::endl 
-		  << "---------------------------------------------------------------------"
-		  << std::endl << std::asctime(std::localtime(&result))
-		  << std::endl
-		  << "Threshold for finite field Strassen-Winograd matrix multiplication" ;
-	F.write(cerr << " (using ") << ')' << endl << endl;
+    time_t result = std::time(NULL);
+    cerr << std::endl
+    << "---------------------------------------------------------------------"
+    << std::endl << std::asctime(std::localtime(&result))
+    << std::endl
+    << "Threshold for finite field Strassen-Winograd matrix multiplication" ;
+    F.write(cerr << " (using ") << ')' << endl << endl;
 
-	cerr << "fgemm:  n                   Classic                        Winograd 1 level" << std::endl;
-	cerr << "                    seconds            Gfops          seconds            Gfops" << std::endl;
-	FFLAS::MMHelper<Field, FFLAS::MMHelperAlgo::Winograd> ClassicH(F,0, FFLAS::ParSeqHelper::Sequential());
-	FFLAS::MMHelper<Field, FFLAS::MMHelperAlgo::Winograd> WinogradH(F,1, FFLAS::ParSeqHelper::Sequential());
-	    //warm up computation
-	FFLAS::fgemm(F, FFLAS::FflasNoTrans, FFLAS::FflasNoTrans, n, n, n, F.mOne, A, n, B, n, F.one, C, n, ClassicH);
-	do {
-		double classicTime, winogradTime;
+    cerr << "fgemm:  n                   Classic                        Winograd 1 level" << std::endl;
+    cerr << "                    seconds            Gfops          seconds            Gfops" << std::endl;
+    FFLAS::MMHelper<Field, FFLAS::MMHelperAlgo::Winograd> ClassicH(F,0, FFLAS::ParSeqHelper::Sequential());
+    FFLAS::MMHelper<Field, FFLAS::MMHelperAlgo::Winograd> WinogradH(F,1, FFLAS::ParSeqHelper::Sequential());
+    //warm up computation
+    FFLAS::fgemm(F, FFLAS::FflasNoTrans, FFLAS::FflasNoTrans, n, n, n, F.mOne, A, n, B, n, F.one, C, n, ClassicH);
+    do {
+        double classicTime, winogradTime;
 
-		int iter=3;
-		chrono.start();
-		for (int i=0;i<iter;i++)
-			FFLAS::fgemm(F, FFLAS::FflasNoTrans, FFLAS::FflasNoTrans, n, n, n, F.mOne, A, n, B, n, F.one, C, n, ClassicH);
-		chrono.stop();
+        int iter=3;
+        chrono.start();
+        for (int i=0;i<iter;i++)
+            FFLAS::fgemm(F, FFLAS::FflasNoTrans, FFLAS::FflasNoTrans, n, n, n, F.mOne, A, n, B, n, F.one, C, n, ClassicH);
+        chrono.stop();
 
-		classicTime = chrono.realtime()/iter;
+        classicTime = chrono.realtime()/iter;
 
-		chrono.clear(); chrono.start();
-		for (int i=0; i<iter; i++)
-			FFLAS::fgemm(F, FFLAS::FflasNoTrans, FFLAS::FflasNoTrans, n, n, n, F.mOne, A, n, B,n, F.one, C, n, WinogradH);
-		chrono.stop();
-		
-		winogradTime = chrono.realtime()/iter;
+        chrono.clear(); chrono.start();
+        for (int i=0; i<iter; i++)
+            FFLAS::fgemm(F, FFLAS::FflasNoTrans, FFLAS::FflasNoTrans, n, n, n, F.mOne, A, n, B,n, F.one, C, n, WinogradH);
+        chrono.stop();
 
-		cerr << "      ";
-		cerr.width(4);
-		cerr << n;
-		cerr << "  ";
-		cerr.width(15);
-		cerr << classicTime;
-		cerr << "  ";
-		cerr.width(15);
-		cerr << GFOPS(n, classicTime) << "  ";
-		cerr.width(15);
-		cerr << winogradTime;
-		cerr << "  ";
-		cerr.width(15);
-		cerr << GFOPS(n, winogradTime) << endl;
+        winogradTime = chrono.realtime()/iter;
 
-		if (classicTime > winogradTime ){
-			count++;
-			if (count > 1){
-				nbest = n;
-				bound = true;
-				prec = prec >> 1;
-				n -= prec;
-			}
-		}
-		else{
-			count=0;
-			if (bound)
-				prec=prec>>1;
-			n+=prec;
-		}
-	} while ((prec > 32 ) && (n < nmax));
+        cerr << "      ";
+        cerr.width(4);
+        cerr << n;
+        cerr << "  ";
+        cerr.width(15);
+        cerr << classicTime;
+        cerr << "  ";
+        cerr.width(15);
+        cerr << GFOPS(n, classicTime) << "  ";
+        cerr.width(15);
+        cerr << winogradTime;
+        cerr << "  ";
+        cerr.width(15);
+        cerr << GFOPS(n, winogradTime) << endl;
 
-	cerr<<endl;
-	if (nbest != 0 ) {
-		if (typeid(Element).name() == typeid(double).name()) {
-			if ( balanced(F) ) {
-				cout << "#ifndef __FFLASFFPACK_WINOTHRESHOLD_BAL"  << endl;
-				cout << "#define __FFLASFFPACK_WINOTHRESHOLD_BAL" << ' ' <<  nbest << endl;
-				cerr << "defined __FFLASFFPACK_WINOTHRESHOLD_BAL to " << nbest << "" << std::endl;
-		}
-			else {
-				cout << "#ifndef __FFLASFFPACK_WINOTHRESHOLD"  << endl;
-				cout << "#define __FFLASFFPACK_WINOTHRESHOLD" << ' ' <<  nbest << endl;
-				cerr << "defined __FFLASFFPACK_WINOTHRESHOLD to " << nbest << "" << std::endl;
-	
-			}
-			std::cout << "#endif" << endl  << endl;
-		}
-		
-		if (typeid(Element).name() == typeid(float).name()) {
-			if ( balanced(F) ) {
-				cout << "#ifndef __FFLASFFPACK_WINOTHRESHOLD_BAL_FLT"  << endl;
-				cout << "#define __FFLASFFPACK_WINOTHRESHOLD_BAL_FLT" << ' ' << nbest << endl;
-				cerr << "defined __FFLASFFPACK_WINOTHRESHOLD_BAL_FLT to " << nbest << "" << std::endl;
+        if (classicTime > winogradTime ){
+            count++;
+            if (count > 1){
+                nbest = n;
+                bound = true;
+                prec = prec >> 1;
+                n -= prec;
+            }
+        }
+        else{
+            count=0;
+            if (bound)
+                prec=prec>>1;
+            n+=prec;
+        }
+    } while ((prec > 32 ) && (n < nmax));
 
-			}
-			else {
-				cout << "#ifndef __FFLASFFPACK_WINOTHRESHOLD_FLT"  << endl;
-				cout << "#define __FFLASFFPACK_WINOTHRESHOLD_FLT" << ' ' << nbest << endl;
-				cerr << "defined __FFLASFFPACK_WINOTHRESHOLD_FLT to " << nbest << "" << std::endl;
-			}
-			cout << "#endif" << endl << endl;
-		}
-	}
+    cerr<<endl;
+    if (nbest != 0 ) {
+        if (typeid(Element).name() == typeid(double).name()) {
+            if ( balanced(F) ) {
+                cout << "#ifndef __FFLASFFPACK_WINOTHRESHOLD_BAL"  << endl;
+                cout << "#define __FFLASFFPACK_WINOTHRESHOLD_BAL" << ' ' <<  nbest << endl;
+                cerr << "defined __FFLASFFPACK_WINOTHRESHOLD_BAL to " << nbest << "" << std::endl;
+            }
+            else {
+                cout << "#ifndef __FFLASFFPACK_WINOTHRESHOLD"  << endl;
+                cout << "#define __FFLASFFPACK_WINOTHRESHOLD" << ' ' <<  nbest << endl;
+                cerr << "defined __FFLASFFPACK_WINOTHRESHOLD to " << nbest << "" << std::endl;
 
-	FFLAS::fflas_delete(A);
-	FFLAS::fflas_delete(B);
-	FFLAS::fflas_delete(C);
+            }
+            std::cout << "#endif" << endl  << endl;
+        }
 
-	return 0;
+        if (typeid(Element).name() == typeid(float).name()) {
+            if ( balanced(F) ) {
+                cout << "#ifndef __FFLASFFPACK_WINOTHRESHOLD_BAL_FLT"  << endl;
+                cout << "#define __FFLASFFPACK_WINOTHRESHOLD_BAL_FLT" << ' ' << nbest << endl;
+                cerr << "defined __FFLASFFPACK_WINOTHRESHOLD_BAL_FLT to " << nbest << "" << std::endl;
+
+            }
+            else {
+                cout << "#ifndef __FFLASFFPACK_WINOTHRESHOLD_FLT"  << endl;
+                cout << "#define __FFLASFFPACK_WINOTHRESHOLD_FLT" << ' ' << nbest << endl;
+                cerr << "defined __FFLASFFPACK_WINOTHRESHOLD_FLT to " << nbest << "" << std::endl;
+            }
+            cout << "#endif" << endl << endl;
+        }
+    }
+
+    FFLAS::fflas_delete(A);
+    FFLAS::fflas_delete(B);
+    FFLAS::fflas_delete(C);
+
+    return 0;
 }
+/* -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+// vim:sts=4:sw=4:ts=4:et:sr:cino=>s,f0,{0,g0,(0,\:0,t0,+0,=s
