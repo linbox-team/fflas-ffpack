e723e6a8245d04a8ec83a7304b1704dd887d5b9e
Clément Pernet <ClementPernet@users.noreply.github.com>
2019-06-21 15:45:24 +0200

    make autotune should fail when some run segfaults so Jenkins can detect it (#270)

--- autotune/tune_fgemm.sh
+++ autotune/tune_fgemm.sh
@@ -1,16 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 echo =================================================
 echo ========= FFLAS-FFPACK fgemm Autotuning =========
 echo =================================================
 echo 
 echo "== Tuning fgemm over Modular<double> =="
 (./winograd-modular-double > fgemm-thresholds.h) 2>&1 | tee fgemm-autotune.log
+val=${PIPESTATUS[0]}; if test ${val} -ne 0 ; then exit ${val}; fi
 echo 
 echo "== Tuning fgemm over Modular<float> =="
 (./winograd-modular-float >>  fgemm-thresholds.h) 2>&1 | tee -a fgemm-autotune.log
+val=${PIPESTATUS[0]}; if test ${val} -ne 0 ; then exit ${val}; fi
 echo 
 echo "== Tuning fgemm over ModularBalanced<double> =="
 (./winograd-modularbalanced-double >> fgemm-thresholds.h) 2>&1 | tee -a fgemm-autotune.log
+val=${PIPESTATUS[0]}; if test ${val} -ne 0 ; then exit ${val}; fi
 echo 
 echo "== Tuning fgemm over ModularBalanced<float> =="
 (./winograd-modularbalanced-float >>  fgemm-thresholds.h) 2>&1 | tee -a fgemm-autotune.log
+val=${PIPESTATUS[0]}; if test ${val} -ne 0 ; then exit ${val}; fi
