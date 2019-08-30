1c24463a60abda85ef102a64b8dfe53858a6c55a
Jean-Guillaume Dumas <Jean-Guillaume.Dumas@imag.fr>
2019-08-30 11:46:46 +0200

    VALUE instead of firstprivate

--- fflas-ffpack/paladin/parallel.h
+++ fflas-ffpack/paladin/parallel.h
@@ -208,17 +208,18 @@ WAIT;
 FORBLOCK1D(_internal_iterator, m, Helper,                           \
            const auto internal_iter_begin(_internal_iterator.begin());      \
            const auto internal_iter_end(_internal_iterator.end());      \
-           TASK(firstprivate(internal_iter_begin, internal_iter_end), \
+           TASK(VALUE(internal_iter_begin, internal_iter_end), \
                  {for(auto i=internal_iter_begin; i!=internal_iter_end; ++i) \
                  { Args; } });)                                         \
                  WAIT;
 
 
+// WARNING: @fixme, the passed mode should not contain another VALUE mode
 #define FOR1D_2(i, m, Helper, mode, Args ...)                             \
 FORBLOCK1D(_internal_iterator, m, Helper,                           \
            const auto internal_iter_begin(_internal_iterator.begin());      \
            const auto internal_iter_end(_internal_iterator.end());      \
-           TASK(firstprivate(internal_iter_begin, internal_iter_end) mode, \
+           TASK(VALUE(internal_iter_begin, internal_iter_end) mode, \
                  {for(auto i=internal_iter_begin; i!=internal_iter_end; ++i) \
                  { Args; } });)                                         \
                  WAIT;
