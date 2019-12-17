aa5e3971de42e71f873a209aa6472c1570ed7b97
A. Breust <alexis.breust@gmail.com>
2019-12-17 09:39:41 +0100

    Added firstprivate for iterator to TASK in FOR1D (#280)
    * Added firstprivate for iterator to TASK in FOR1D
    
    * VALUE instead of firstprivate

--- fflas-ffpack/paladin/parallel.h
+++ fflas-ffpack/paladin/parallel.h
@@ -208,17 +208,18 @@ WAIT;
 FORBLOCK1D(_internal_iterator, m, Helper,                           \
            const auto internal_iter_begin(_internal_iterator.begin());      \
            const auto internal_iter_end(_internal_iterator.end());      \
-           TASK( , \
+           TASK(VALUE(internal_iter_begin, internal_iter_end), \
                  {for(auto i=internal_iter_begin; i!=internal_iter_end; ++i) \
                  { Args; } });)                                         \
                  WAIT;
 
 
-#define FOR1D_2(i, m, Helper, mod, Args ...)                             \
+// WARNING: @fixme, the passed mode should not contain another VALUE mode
+#define FOR1D_2(i, m, Helper, mode, Args ...)                             \
 FORBLOCK1D(_internal_iterator, m, Helper,                           \
            const auto internal_iter_begin(_internal_iterator.begin());      \
            const auto internal_iter_end(_internal_iterator.end());      \
-           TASK( mod, \
+           TASK(VALUE(internal_iter_begin, internal_iter_end) mode, \
                  {for(auto i=internal_iter_begin; i!=internal_iter_end; ++i) \
                  { Args; } });)                                         \
                  WAIT;
