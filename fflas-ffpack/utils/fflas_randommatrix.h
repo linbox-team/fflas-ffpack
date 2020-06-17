4e71d7916e7637a74468badbb71de03c96617e08
Clément Pernet <clement.pernet@gmail.com>
2020-06-17 13:19:18 +0200

    randgen now works

--- fflas-ffpack/utils/fflas_randommatrix.h
+++ fflas-ffpack/utils/fflas_randommatrix.h
@@ -419,22 +419,32 @@ namespace FFPACK{
         // }
 
         size_t fullRkBlocks;
+        size_t p;
         do {
+//            std::cerr<<"Iteration n,r,t="<<n<<" "<<r<<" "<<t<<std::endl;
             std::vector<bool> pivot_in_row (n,false);
             std::vector<bool> pivot_in_col (n,false);
-            size_t p=0;
+            p=0;
             for (size_t i=0; i<n; ++i) leadingRk[i]=0;
             fullRkBlocks = 0;
             while (p<r && fullRkBlocks < n-1){ // finish the last pivots by random samples
                 size_t i = RandInt (0,n-1);
                 size_t j = RandInt (0,n-1);
+                    //std::cerr<<"Pick ("<<i<<", "<<j<<")"<<std::endl;
                 if (j > n-i-1){ // flip right and left triangular part
-                    std::swap (i,j);
+                        //                  std::cerr<<"i,j,n-i-1 = "<<i<<" "<<j<<" "<<(n-i-1)<<std::endl;
+                    size_t tmp = n-j-1;
+                    j = n-i-1;
+                    i=tmp;
+//                    std::cerr<<"Right -> Left  ("<<i<<", "<<j<<")"<<std::endl;
+
                 }
+                if (j==n-i-1) continue;
                 if (!pivot_in_row[i] && !pivot_in_col[j]){
                     bool admissible = true;
                     for (size_t k=i; k<n-j-1; k++)
                         admissible = admissible && (leadingRk[k] < t);
+                        //                  if (admissible) std::cerr<<"Admissible = "<<std::endl;
                     if (admissible){
                         rows[p] = i;
                         cols[p] = j;
@@ -448,7 +458,9 @@ namespace FFPACK{
                     }
                 }
             }
-        } while (fullRkBlocks == n-1);
+//            if (p<r && fullRkBlocks == n-1) std::cerr<<"No room left -> starting over again"<<std::endl;
+        } while (p<r && fullRkBlocks == n-1);
+            //      std::cerr<<"done"<<std::endl;
         FFLAS::fflas_delete (leadingRk);
 //        FFLAS::fflas_delete (randcols,randrows);
     }
