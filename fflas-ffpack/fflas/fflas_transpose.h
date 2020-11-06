79c12210b0c1acbedc3aaa428eb0e2c5ee5c6cf6
git <pascal.giorgi@lirmm.fr>
2020-11-06 17:54:53 +0100

    provide faster variant of transpose with SIMD

--- fflas-ffpack/fflas/fflas_transpose.h
+++ fflas-ffpack/fflas/fflas_transpose.h
@@ -69,6 +69,145 @@ namespace FFLAS {
 
 };
 
+  template <typename Field, typename Simd, size_t>
+  struct transpose_simd {
+    void operator()(const Field&F, typename Field::ConstElement_ptr A, size_t lda, typename Field::Element_ptr B, size_t ldb);
+  };
+  
+  
+  template <typename Field, typename Simd>
+  struct transpose_simd<Field, Simd, 4> {
+    inline void operator()(const Field&F, typename Field::ConstElement_ptr A, size_t lda, typename Field::Element_ptr B, size_t ldb){
+      typename Simd::vect_t R0,R1,R2,R3;
+      R0= Simd::loadu(A);
+      R1= Simd::loadu(A+lda);
+      R2= Simd::loadu(A+(lda<<1));
+      R3= Simd::loadu(A+lda*3);
+      Simd::unpacklohi(R0,R2,R0,R2);
+      Simd::unpacklohi(R1,R3,R1,R3);
+      Simd::unpacklohi(R0,R1,R0,R1);
+      Simd::unpacklohi(R2,R3,R2,R3);
+      Simd::storeu(B       , R0);
+      Simd::storeu(B+ldb   , R1);
+      Simd::storeu(B+(ldb<<1), R2);
+      Simd::storeu(B+ldb*3 , R3);
+    }
+  };
+
+  template <typename Field, typename Simd>
+  struct transpose_simd<Field, Simd, 8> {
+    inline void operator() (const Field&F, typename Field::ConstElement_ptr A, size_t lda, typename Field::Element_ptr B, size_t ldb){
+      typename Simd::vect_t R0,R1,R2,R3,R4,R5,R6,R7;
+      R0= Simd::loadu(A);
+      R1= Simd::loadu(A+lda);
+      R2= Simd::loadu(A+(lda<<1));
+      R3= Simd::loadu(A+lda*3);
+      R4= Simd::loadu(A+(lda<<2));
+      R5= Simd::loadu(A+lda*5);
+      R6= Simd::loadu(A+lda*6);
+      R7= Simd::loadu(A+lda*7);
+
+      Simd::unpacklohi(R0,R4,R0,R4);
+      Simd::unpacklohi(R1,R5,R1,R5);
+      Simd::unpacklohi(R2,R6,R2,R6);
+      Simd::unpacklohi(R3,R7,R3,R7);
+
+      Simd::unpacklohi(R0,R2,R0,R2);
+      Simd::unpacklohi(R1,R3,R1,R3);
+      Simd::unpacklohi(R4,R6,R4,R6);
+      Simd::unpacklohi(R5,R7,R5,R7);
+
+      Simd::unpacklohi(R0,R1,R0,R1);
+      Simd::unpacklohi(R2,R3,R2,R3);
+      Simd::unpacklohi(R4,R5,R4,R5);
+      Simd::unpacklohi(R6,R7,R6,R7);
+
+      Simd::storeu(B         , R0);
+      Simd::storeu(B+ldb     , R1);
+      Simd::storeu(B+(ldb<<1), R2);
+      Simd::storeu(B+ldb*3   , R3);
+      Simd::storeu(B+(ldb<<2), R4);
+      Simd::storeu(B+ldb*5   , R5);
+      Simd::storeu(B+ldb*6   , R6);
+      Simd::storeu(B+ldb*7   , R7);
+    }
+  };
+  template <typename Field, typename Simd>
+  struct transpose_simd<Field, Simd, 16> {
+    inline void operator()(const Field&F, typename Field::ConstElement_ptr A, size_t lda, typename Field::Element_ptr B, size_t ldb){
+      typename Simd::vect_t R0,R1,R2,R3,R4,R5,R6,R7,R8,R9,R10,R11,R12,R13,R14,R15;
+      R0= Simd::loadu(A);
+      R1= Simd::loadu(A+lda);
+      R2= Simd::loadu(A+(lda<<1));
+      R3= Simd::loadu(A+lda*3);
+      R4= Simd::loadu(A+(lda<<2));
+      R5= Simd::loadu(A+lda*5);
+      R6= Simd::loadu(A+lda*6);
+      R7= Simd::loadu(A+lda*7);
+      R8= Simd::loadu(A+(lda<<3));
+      R9= Simd::loadu(A+lda*9);
+      R10= Simd::loadu(A+lda*10);
+      R11= Simd::loadu(A+lda*11);
+      R12= Simd::loadu(A+lda*12);
+      R13= Simd::loadu(A+lda*13);
+      R14= Simd::loadu(A+lda*14);
+      R15= Simd::loadu(A+lda*15);
+
+      Simd::unpacklohi(R0, R8,R0, R8);
+      Simd::unpacklohi(R1, R9,R1, R9);
+      Simd::unpacklohi(R2,R10,R2,R10);
+      Simd::unpacklohi(R3,R11,R3,R11);
+      Simd::unpacklohi(R4,R12,R4,R12);
+      Simd::unpacklohi(R5,R13,R5,R13);
+      Simd::unpacklohi(R6,R14,R6,R14);
+      Simd::unpacklohi(R7,R15,R7,R15);
+
+      Simd::unpacklohi(R0,  R4,R0,  R4);
+      Simd::unpacklohi(R1,  R5,R1,  R5);
+      Simd::unpacklohi(R2,  R6,R2,  R6);
+      Simd::unpacklohi(R3,  R7,R3,  R7);
+      Simd::unpacklohi(R8, R12,R8, R12);
+      Simd::unpacklohi(R9, R13,R9, R13);
+      Simd::unpacklohi(R10,R14,R10,R14);
+      Simd::unpacklohi(R11,R15,R11,R15);
+
+      Simd::unpacklohi(R0,  R2,R0,  R2);
+      Simd::unpacklohi(R1,  R3,R1,  R3);
+      Simd::unpacklohi(R4,  R6,R4,  R6);
+      Simd::unpacklohi(R5,  R7,R5,  R7);
+      Simd::unpacklohi(R8, R10,R8, R10);
+      Simd::unpacklohi(R9, R11,R9, R11);
+      Simd::unpacklohi(R12,R14,R12,R14);
+      Simd::unpacklohi(R13,R15,R13,R15);
+
+      Simd::unpacklohi(R0,  R1,R0,  R1);
+      Simd::unpacklohi(R2,  R3,R2,  R3);
+      Simd::unpacklohi(R4,  R5,R4,  R5);
+      Simd::unpacklohi(R6,  R7,R6,  R7);
+      Simd::unpacklohi(R8,  R9,R8,  R9);
+      Simd::unpacklohi(R10,R11,R10,R11);
+      Simd::unpacklohi(R12,R13,R12,R13);
+      Simd::unpacklohi(R14,R15,R14,R15);
+
+      Simd::storeu(B         , R0);
+      Simd::storeu(B+ldb     , R1);
+      Simd::storeu(B+(ldb<<1), R2);
+      Simd::storeu(B+ldb*3   , R3);
+      Simd::storeu(B+(ldb<<2), R4);
+      Simd::storeu(B+ldb*5   , R5);
+      Simd::storeu(B+ldb*6   , R6);
+      Simd::storeu(B+ldb*7   , R7);
+      Simd::storeu(B+(ldb<<3), R8);
+      Simd::storeu(B+ldb*9   , R9);
+      Simd::storeu(B+ldb*10  , R10);
+      Simd::storeu(B+ldb*11  , R11);
+      Simd::storeu(B+ldb*12  , R12);
+      Simd::storeu(B+ldb*13  , R13);
+      Simd::storeu(B+ldb*14  , R14);
+      Simd::storeu(B+ldb*15  , R15);
+    }
+  };
+  
 
   template <typename Field, typename Simd>
   class BlockTransposeSIMD<Field,Simd,
@@ -78,16 +217,20 @@ namespace FFLAS {
     using Element_ptr      = typename Field::Element_ptr;
     using ConstElement_ptr = typename Field::ConstElement_ptr;
     using vect_t           = typename Simd::vect_t;
-
     
-    void transposein(const Field&F, Element_ptr A, size_t lda){
+    void transposein(const Field&F, Element_ptr A, size_t lda)
+    {
       transpose(F, A,lda,A,lda);
     }
 
-    void transpose(const Field&F, ConstElement_ptr A, size_t lda, Element_ptr B, size_t ldb){
+    void transpose(const Field&F, ConstElement_ptr A, size_t lda, Element_ptr B, size_t ldb)
+    {
+      transpose_simd<Field, Simd, Simd::vect_size>() (F,A,lda,B,ldb);
+    }
+    void transposeLoop(const Field&F, ConstElement_ptr A, size_t lda, Element_ptr B, size_t ldb){
       vect_t R[Simd::vect_size];
       for (size_t i=0;i<Simd::vect_size;i++)
-	R[i] = Simd::loadu(A+i*lda);
+    	R[i] = Simd::loadu(A+i*lda);
 
       //WriteMatrix (std::cerr,F,size(),size(),A,lda)<<std::endl<<"iiiiiiiiii\n";
 
@@ -95,16 +238,16 @@ namespace FFLAS {
       size_t f=1;
       size_t idx0,idx1;
       for (;w>0; w>>=1, f<<=1){
-	idx0=0;idx1=w;
-	for (size_t i = 0; i < f; i++, idx0+=w,idx1+=w)
-	  for (size_t j = 0; j < w; j++, idx0++, idx1++)
-	    {
-	      //std::cerr<< idx0 <<" <-> "<<idx1<<std::endl;
-	      Simd::unpacklohi(R[idx0],R[idx1],R[idx0],R[idx1]);
-	    }
+    	idx0=0;idx1=w;
+    	for (size_t i = 0; i < f; i++, idx0+=w,idx1+=w)
+    	  for (size_t j = 0; j < w; j++, idx0++, idx1++)
+    	    {
+    	      //std::cerr<< idx0 <<" <-> "<<idx1<<std::endl;
+    	      Simd::unpacklohi(R[idx0],R[idx1],R[idx0],R[idx1]);
+    	    }
       }      
       for (size_t i=0;i<Simd::vect_size;i++)
-	Simd::storeu(B+i*ldb, R[i]);
+    	Simd::storeu(B+i*ldb, R[i]);
     }
 
      const constexpr size_t size() const { return Simd::vect_size;}
@@ -114,8 +257,7 @@ namespace FFLAS {
     }
   };
 
-
-  
+ 
 
   template<class Field, size_t BLOCK>
   inline  typename Field::Element_ptr
